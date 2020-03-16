0e63cdad3a80f0f8a7081b68c7c514103c75639a
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2020-03-16 13:51:49 -0300

    Create paramSubset.R
    Subsetting by parameter. Now need to add this to the subset method in AllClass.R

---
+++ sandbox/jlh/03_parameterSubset/paramSubset.R
@@ -0,0 +1,32 @@
+#Setup
+
+library(argoFloats)
+library(oce)
+
+#Download Index
+getIndex(
+    server = "ftp://usgodae.org/pub/outgoing/argo",
+    file = "argo_bio-profile_index.txt",
+    destdir = "~/data/argo",
+    age = 6,
+    quiet = FALSE,
+    debug = 3
+)
+
+if (!exists("ai"))
+    ai <- getIndex(file='argo_bio-profile_index.txt', destdir="~/data/argo",debug=3, age=0)
+
+
+load("~/data/argo/argo_bio-profile_index.txt") 
+str(argoFloatsIndex,1) #Clarify argoFloatsIndex
+str(argoFloatsIndex$index) # Want to be working with the index (it's the data frame)
+head(argoFloatsIndex$index$parameters,3) # To focus on parameters
+look<- grepl("([ ]DOXY)|(^DOXY)", argoFloatsIndex$index$parameters) # This is using logical grep, saying if something is true or not.It also uses regexp to make sure it is only getting DOXY, and not, for example, BPAHSE_DOXY
+#look[1:3]
+lines[look] # to solely write the lines that have isolates what we want.
+
+# To find out how many:
+
+sum(look)/ length(look) # This indicates there is a lot of oxygen (94%). 
+
+# Now it's time to add this to subset method. 
