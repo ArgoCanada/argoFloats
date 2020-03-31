04c517f6aa56fa820ff80ee4f4ad8ab796391eeb
dankelley <kelley.dan@gmail.com>
2020-03-31 16:24:55 -0300

    add test for subset(..., rectangle=) method

--- tests/testthat/test_subset.R
+++ tests/testthat/test_subset.R
@@ -7,7 +7,20 @@ context("subset")
 
 test_that("subset by circle", {
           data("index")
-          indexAI <- expect_silent(subset(index, circle=list(longitude=-77.06, latitude=26.54, radius=100)))
-          expect_equal(dim(indexAI[["index"]]), c(304, 8))
+          indexc <- expect_silent(subset(index, circle=list(longitude=-77.06, latitude=26.54, radius=100)))
+          expect_equal(dim(indexc[["index"]]), c(304, 8))
 })
+
+test_that("subset by rectangle", {
+          data("index")
+          indexr <- expect_silent(subset(index,
+                                         rectangle=list(longitude=c(-77, -76),
+                                                        latitude=c(25, 26))))
+          expect_equal(dim(indexr[["index"]]), c(76, 8))
+          expect_equal(indexr[["index"]][["file"]][1], "aoml/4900183/profiles/D4900183_024.nc")
+})
+
+# Add test for polygon method here
+
+# Add test for time method here
 
