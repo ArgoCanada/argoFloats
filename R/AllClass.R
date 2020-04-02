29aaf6004110b2c98aa8c0b3523cca85f463052b
dankelley <kelley.dan@gmail.com>
2020-04-02 18:21:12 -0300

    fix subset by polygon +
    1. revise subset examples to use the new plot() method
    2. revise subset examples to use built-in datasets
    3. make subset() report number of profiles, as well as percentage
    4. alter tests to account for #3 above
    5. alter subset() code to use 'keep' variable in all cases, for clarity
    6. some other things that I don't remember too well, given on/off
    working, due to rolling power failures today

--- R/AllClass.R
+++ R/AllClass.R
@@ -330,19 +330,19 @@ setMethod(f="summary",
 #' 1. A list named `circle` with elements named `longitude`,
 #' `latitude` and `radius`.  The first two give the center of
 #' the subset region, and the third gives the radius of
-#' that region, in kilometers.
+#' that region, in kilometers. See Example 2A.
 #' 2. A list named `rectangle`, which has an element named `longitude`
 #' with two elements giving the western and eastern limits of the
 #' subset region, and similar one named `latitude` giving the southern
-#' and northern limits.
-#' 3. A list named `parameter`, which has `argo`, `argo_bgc`, or `argo_merge`
-#' parameter elements indicating the parameter of interest.
-#' 4. A list named `polygon` that has elements named `longitude` and `latitude`.
-#' Profiles within this polygon will be retained.
+#' and northern limits. See Example 2B.
+#' 3. A list named `polygon` that has elements named `longitude` and `latitude`.
+#' Profiles within this polygon will be retained. See Example 2C.
+#' 4. A vector or list named `parameter` that holds names of parameters to keep;
+#' see examples 3A and 3B.
 #' 5. A list named `time` that has elements `from` and `to` that are POSIXt
 #' times created with eg. [POSIXct()], with `tz="UTC"` to match
 #' the timezone used in Argo data. Profiles within that time frame will
-#' be retained.
+#' be retained. See Example 4.
 #'
 #' In all cases, the notation is that longitude is positive
 #' for degrees East and negative for degrees West, and that latitude
@@ -354,7 +354,7 @@ setMethod(f="summary",
 #' indices of `x@data$index` to keep.  See example 1.
 #'
 #' @param ... a list named `circle`, `rectangle`, `parameter`, `polygon` , or `time`. See \dQuote{Details}
-#' and Example 2.
+#' and \dQuote{Examples}.
 #'
 #' @return An [argoFloats-class] object.
 #'
@@ -363,62 +363,44 @@ setMethod(f="summary",
 #' data(index)
 #'
 #' # Example 1: First three profiles in dataset.
-#' index3 <- subset(index, 1:3)
-#' cat("First 3 longitudes:", paste(index3[["longitude"]]), "\n")
-#' # Example 2: Subsets near Abaca Island
-#' # 2A: circle around the island
-#' indexC <- subset(index, circle=list(longitude=-77.06, latitude=26.54, radius=200))
-#' cat("Found", length(indexC[["longitude"]]), "profiles in circle around Abaca Island\n")
-#' # 2B: rectangle to northeast of the island
-#' indexR <- subset(index, rectangle=list(longitude=c(-77,-76), latitude=c(27,28)))
-#' cat("Found", length(indexR[["longitude"]]), "profiles in rectangle north of Abaca Island\n")
+#' index1 <- subset(index, 1:3)
+#' cat("First 3 longitudes:", paste(index1[["longitude"]]), "\n")
+#'
+#' # Example 2: Demonstrate geographical subsets. These examples use
+#' # built-in datasets; to use these in other  work, start by
+#' # using getIndex() to create an index.
+#' # 2A: circle north of Abaca Island
+#' index2A <- subset(index, circle=list(longitude=-77.5, latitude=27.5, radius=50))
+#' # 2B: rectangle northeast of Abaca Island
+#' lonRect <- c(-76.5, -76)
+#' latRect <- c(26.5, 27.5)
+#' index2B <- subset(index, rectangle=list(longitude=lonRect, latitude=latRect))
+#' # 2C: polygon to the southeast of Abaco Island
+#' lonPoly <- c(-76.5, -76.0, -75.5)
+#' latPoly <- c(25.5, 26.5, 25.5)
+#' index2C <- subset(index, polygon=list(longitude=lonPoly, latitude=latPoly))
 #' # Show these subsets on a map
-#' library(oce)
-#' data(coastlineWorldFine, package="ocedata")
-#' par(mar=c(2, 2, 1, 1))
-#' plot(coastlineWorldFine, col="tan",
-#'      longitudelim=-77.06+c(-3, 3), latitudelim=26.54+c(-2, 2))
-#' points(indexC[["longitude"]], indexC[["latitude"]], col="red")
-#' points(indexR[["longitude"]], indexR[["latitude"]], col="blue", pch=20)
-#' # 2C: Polygon to the northeast of the Abaco island
-#' \dontrun{
-#' library(oce)
-#' library(sp)
-#' data(package='ocedata')
-#' data('coastlineWorldFine', package='ocedata')
-#' latitudePolygon <- c(24, 27, 24) # Creating a polygon that closes
-#' longitudePolygon <- c(-79, -78, -74 )
-#' mapPlot(coastlineWorldFine, col='lightgray', longitudelim=c(-83,-71),
-#'         latitudelim=c(20,30), projection="+proj=merc", grid=TRUE)
-#' mapPoints(longitudePolygon, latitudePolygon, type="l", lwd=5, col="blue")
-#' abacaSub <- subset(index, circle=list(longitude=-77.15, latitude=26.35,
-#'                    radius=300))
-#' latitudePoint <- abacaSub[['latitude']]
-#' longitudePoint <- abacaSub[['longitude']]
-#' indexP <- subset(index, polygon=list(latitude=latitudePolygon,
-#'                  longitude=longitudePolygon))
-#' inside <- sp::point.in.polygon(longitudePoint, latitudePoint,
-#'                                longitudePolygon, latitudePolygon)
-#' mapPoints(longitudePoint, latitudePoint, col=inside+1, pch=20) }
+#' plot(index)
+#' points(index2A[["longitude"]], index2A[["latitude"]], col=2, pch=20, cex=1.4)
+#' points(index2B[["longitude"]], index2B[["latitude"]], col=3, pch=20, cex=1.4)
+#' rect(lonRect[1], latRect[1], lonRect[2], latRect[2], border=3, lwd=2)
+#' points(index2C[["longitude"]], index2C[["latitude"]], col=4, pch=20, cex=1.4)
+#' polygon(lonPoly, latPoly, border=4, lwd=2)
+#'
 #' # Example 3: Subsetting argo_merge data containing 'DOXY' parameters
-#' # 3A: Data containing all 'DOXY' parameters (single or full word)
-#' ai <- getIndex(file='merge', destdir='~/data/argo')
-#' summary(ai)
-#' aiDoxy <- subset(ai, parameter="DOXY")
-#' summary(aiDoxy)
-#' # 3B: Data containing strictly 'DOXY' parameters (single word)
-#' \dontrun{
-#' ai <- getIndex(file='merge', destdir='~/data/argo')
-#' subDoxy <- subset(ai, parameter='\\bDOXY\\b')
-#' summary(subDoxy)}
+#' # 3A: Data containing 'DOXY' data
+#' data(indexMerged)
+#' index3A <- subset(indexMerged, parameter="DOXY")
+#' # 3B: Data containing both 'PSAL' and 'DOWN_IRRADIANCE380' data
+#' data(indexMerged)
+#' index3B <- subset(indexMerged, parameter=c("PSAL", "DOWN_IRRADIANCE380"))
+#'
 #' # Example 4: Subsetting data for the year 2019
-#' \dontrun{
-#' ai <- getIndex(file='merge', destdir ='~/data/argo')
-#' summary(ai)
+#' data(index)
 #' from <- as.POSIXct("2019-01-01", tz="UTC")
 #' to <- as.POSIXct("2019-12-31", tz="UTC")
-#' ait <- subset(ai, time=list(from=from, to=to))
-#' summary(ait) }
+#' index4 <- subset(index, time=list(from=from, to=to))
+#'
 #' @author Dan Kelley and Jaimie Harbin
 #'
 #' @importFrom oce geodDist
@@ -449,9 +431,8 @@ setMethod(f="subset",
                       dist <- geodDist(x[["longitude"]], x[["latitude"]], circle$longitude, circle$latitude)
                       keep <- dist < circle$radius
                       keep[is.na(keep)] <- FALSE
-                      if (sum(keep) < 1)
-                          warning("In subset,argoFloats-method(..., circle) : found no profiles within ", circle$radius, "km of ", circle$longitude, "E and ", circle$latitude, "N\n", call.=FALSE)
                       x@data$index <- x@data$index[keep, ]
+                      message("Kept ", sum(keep), " profiles (", round(100*sum(keep)/length(keep),2), "%)")
                   } else if (dotsNames[1] == "rectangle") {
                       rectangle <- dots[[1]]
                       if (!is.list(dots[1]))
@@ -460,58 +441,53 @@ setMethod(f="subset",
                           stop("In subset,argoFloats-method() : 'rectangle' must be a list containing 'longitude' and 'latitude'")
                       keeplon <- rectangle$longitude[1] <=x[["longitude"]] & x[["longitude"]] <= rectangle$longitude[2]
                       keeplat <- rectangle$latitude[1] <= x[["latitude"]] & x[['latitude']] <= rectangle$latitude[2]
-                      keeplon[is.na(keeplon)] <- FALSE
-                      if (sum(keeplon) < 1)
-                          warning("In subset,argoFloats-method(..., rectangle) : found no profiles between given longitudes", call.=FALSE)
-                      keeplat[is.na(keeplat)] <- FALSE
-                      if (sum(keeplat) < 1)
-                          warning("In subset,argoFloats-method(..., rectangle) : found no profiles between given latitudes", call.=FALSE)
-                      x@data$index <- x@data$index[keeplon&keeplat, ]
+                      ok <- is.finite(keeplon) & is.finite(keeplat)
+                      keeplon[!ok] <- FALSE
+                      keeplat[!ok] <- FALSE
+                      keep <- keeplon & keeplat
+                      x@data$index <- x@data$index[keep, ]
+                      message("Kept ", sum(keep), " profiles (", round(100*sum(keep)/length(keep),2), "%)")
                   } else if (dotsNames[1]=="parameter") {
                       parameter <- dots[[1]]
-                      if (!is.list(dots[1]))
-                          stop("In subset,argoFloats-method() : 'parameter' must be a list")
-                      keepparam <- grepl(parameter, x@data$index$parameters)
-                      if (sum(keepparam) < 1)
+                      if (is.list(dots[1]))
+                          parameters <- unlist(parameter)
+                      nparameters <- length(parameters)
+                      parametersList <- lapply(x[["parameters"]], function(p) strsplit(p, " ")[[1]])
+                      keep <- unlist(lapply(parametersList, function(pl) nparameters == sum(parameters %in% pl)))
+                      if (sum(keep) < 1)
                           warning("In subset,argoFloats-method(..., parameter) : found no profiles with given parameter", call.=FALSE)
-                      message("Fraction kept ", round(100*sum(keepparam)/length(keepparam),2), "%.")
-                      x@data$index <- x@data$index[keepparam, ]
+                      message("Kept ", sum(keep), " profiles (", round(100*sum(keep)/length(keep),2), "%)")
+                      x@data$index <- x@data$index[keep, ]
                   } else if (dotsNames[1]=="polygon") {
                       polygon <- dots[[1]]
                       if(!is.list(dots[1]))
                           stop("In subset,argoFloats-method() : 'polygon' must be a list")
-                      keeppoly <- sp::point.in.polygon(x@data$index$longitude,x@data$index$latitude,
-                                                     polygon$longitude, polygon$latitude)
-                      if (sum(keeppoly) < 1)
-                          warning("In subset,argoFloats-method(..., polygon) : found no profiles with given latitude and longitude", call.=FALSE)
-                      message("Fraction kept ", round(100*sum(keeppoly)/length(keeppoly),2), "%.")
-                      x@data$index <- x@data$index[keeppoly, ]
-                 } else if (dotsNames[1]=="time") {
-                     time <- dots[[1]]
+                      keep <- as.logical(sp::point.in.polygon(x[["longitude"]], x[["latitude"]],
+                                                              polygon$longitude, polygon$latitude))
+                      message("Kept ", sum(keep), " profiles (", round(100*sum(keep)/length(keep),2), "%)")
+                      x@data$index <- x@data$index[keep, ]
+                  } else if (dotsNames[1]=="time") {
+                      time <- dots[[1]]
                       if(!is.list(dots[1]))
-                             stop("In subset,argoFloats-method() : 'time' must be a list")
+                          stop("In subset,argoFloats-method() : 'time' must be a list")
                       if (!inherits(time$from, "POSIXt"))
-                         stop("'time' must be a list containing POSIX times")
-                      if (2 != sum(c("from", "to") %in% names(time))) 
-                         stop("In subset,argoFloats-method() : 'time' must be a list containing 'to'and 'from'")
+                          stop("'time' must be a list containing POSIX times")
+                      if (2 != sum(c("from", "to") %in% names(time)))
+                          stop("In subset,argoFloats-method() : 'time' must be a list containing 'to'and 'from'")
                       if (length(time$from) != 1)
-                         stop("from must be of length 1")
+                          stop("from must be of length 1")
                       if (length(time$to) != 1)
                          stop("to must be of length 1")
                       if (time$to <= time$from)
                           stop ("'to' must be greater than 'from'")
-                     keeptime <- time$from[1] <= x[["date"]] & x[["date"]] <= time$to[1]
-                     keeptime[is.na(keeptime)] <- FALSE
-                     #browser()
-                     if (sum(keeptime) < 1)
-                         warning("In subset,argoFloats-method(..., time) : found no profiles within the given time frame", call.=FALSE)
-                     message("Fraction kept ", round(100*sum(keeptime)/length(keeptime),2), "%.")
-                     x@data$index <- x@data$index[keeptime, ]
+                      keep <- time$from[1] <= x[["date"]] & x[["date"]] <= time$to[1]
+                      keep[is.na(keep)] <- FALSE
+                      message("Kept ", sum(keep), " profiles (", round(100*sum(keep)/length(keep),2), "%)")
+                      x@data$index <- x@data$index[keep, ]
                   } else {
                       stop("In subset,argoFloats-method() : the only permitted '...' argument is a list named 'circle','rectangle','parameter','polygon', or 'time'", call.=FALSE)
-                      
                   }
-                  } else {
+              } else {
                   if (length(dotsNames) != 0)
                       stop("in subset,argoFloats-method() : cannot give both 'subset' and '...' arguments", call.=FALSE)
                   if (x@metadata$type == "index") {
