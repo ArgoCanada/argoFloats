e0712ca9cf23e4072b4354a46403049785a0f386
Harbin <HARBINJ@dfo-mpo.ca>
2020-04-03 15:39:24 -0300

    Add show method

---
+++ R/show.R
@@ -0,0 +1,34 @@
+#' Show information about argoFloats object
+#'
+#' @param object an [argoFloats-class] object.
+#'
+#' @export
+#'
+#' @author Jaimie Harbin`
+setMethod(f="show",
+          signature="argoFloats",
+          definition=function(object) {
+              type <- object[['type']]
+              if (type == "index") {
+                  cat('argoFloats object of type "',
+                      object[['type']],
+                      '" with ',
+                      dim(object[['index']])[1], 
+                      " items",
+                      sep="") 
+              } else if (type == "profiles") {
+                  cat('argoFloats object of type "',
+                      object[['type']],
+                      '" with ',
+                      length(object@data$file), 
+                      " items",
+                      sep="") 
+              } else if (type == "argos") { 
+                  cat('argoFloats object of type "',
+                      object[['type']],
+                      '" with ',
+                      length(object@data$argos), 
+                      " items",
+                      sep="")
+              } else stop("Unknown type =", type)
+          })
