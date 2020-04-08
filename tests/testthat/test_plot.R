6b3dfd534f784990f155077411d7c5216126770d
dankelley <kelley.dan@gmail.com>
2020-04-08 16:31:31 -0300

    add tests for show, summary, and plot

---
+++ tests/testthat/test_plot.R
@@ -0,0 +1,12 @@
+## vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
+
+library(argoFloats)
+library(testthat)
+
+context("plot")
+
+test_that("plot", {
+          data("index")
+          expect_silent(plot(index, which="map"))
+})
+
---
+++ tests/testthat/test_show.R
@@ -0,0 +1,13 @@
+## vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
+## The tests relating to data dimensions will need to be altered if the dataset is altered.
+
+library(argoFloats)
+library(testthat)
+
+context("show")
+
+test_that("show", {
+          data("index")
+          expect_output(print(index), "argoFloats object of type \"index\" with 953 items")
+})
+
