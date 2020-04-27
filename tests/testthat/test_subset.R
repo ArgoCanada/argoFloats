d918db7b0a1c8324bccc7cb3baa5268c16a63fbb
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2020-04-27 13:23:44 -0300

    polygon warning
    changed polygon warning back

--- tests/testthat/test_subset.R
+++ tests/testthat/test_subset.R
@@ -28,7 +28,7 @@ test_that("subset by rectangle", {
 test_that("subset by polygon", {
           data("index")
           indexp <- expect_warning(expect_message(subset(index, polygon=list(latitude=c(25,27,25), longitude=c(-78,-77,-74))),
-                                   "Kept 379 profiles \\(40%\\)"), "First and last points did not match. Closing the polygon to avoid crossing line segments.")
+                                   "Kept 379 profiles \\(40%\\)"), "Closing the polygon, since the first and last points did not match.")
           expect_equal(dim(indexp[["index"]]), c(379,8))
           expect_equal(indexp[["index"]][["file"]][1], "aoml/3900582/profiles/D3900582_069.nc")
           expect_equal(indexp[["file"]][1], "aoml/3900582/profiles/D3900582_069.nc")
