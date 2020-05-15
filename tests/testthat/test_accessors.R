3ec0851000e8a843adb66d9f422ea08015dda6d4
dankelley <kelley.dan@gmail.com>
2020-05-15 14:45:33 -0300

    address issues 90 and 91
    The main user-level changes are:
    1. we can do index[["ID"]]
    2. index[["profile"]] now gives the profile number
    3. index[["file"]] gives the file name

--- tests/testthat/test_accessors.R
+++ tests/testthat/test_accessors.R
@@ -9,10 +9,21 @@ data(indexBgc)
 data(indexMerged)
 
 test_that("accessors work on 'index' data file", {
-          expect_equal(index[["profile", 1]], "aoml/1901584/profiles/R1901584_124.nc")
-          expect_equal(index[["profile", 1:2]], c("aoml/1901584/profiles/R1901584_124.nc", "aoml/1901584/profiles/R1901584_125.nc"))
+          expect_equal(index[["file", 1]], "aoml/1901584/profiles/R1901584_124.nc")
+          expect_equal(index[["file", 1:2]], c("aoml/1901584/profiles/R1901584_124.nc", "aoml/1901584/profiles/R1901584_125.nc"))
           expect_equal(index[[1]]$file, index[["file"]][1])
           expect_equal(head(index[["longitude"]],3), c(-76.231, -76.024, -76.320))
           expect_equal(head(index[["latitude"]],3), c(27.705, 27.773, 28.064))
+})
+
+test_that("access float ID", {
+          expect_equal("4900227", index[['ID', 953]])
+          expect_equal(c("1901584", "1901584", "1901584"), index[['ID', 1:3]])
+          expect_equal(c("1901584", "1901584", "1901584"), head(index[['ID']], 3))
+})
+
+test_that("access float profile number", {
+          expect_equal(c("124", "125", "126"), index[["profile", 1:3]])
+          expect_equal(c("124", "125", "126"), head(index[["profile"]], 3))
 })
 
