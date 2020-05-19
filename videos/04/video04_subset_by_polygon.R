d3ea6518fb9327dd067bcec9af9e80f1d099776a
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2020-05-19 14:32:02 -0300

    Update video04_subset_by_polygon.R
    fixed typo in code

--- videos/04/video04_subset_by_polygon.R
+++ videos/04/video04_subset_by_polygon.R
@@ -4,9 +4,9 @@ s <- subset(ai, circle=list(longitude=-83, latitude=9, radius=500))
 plot(s, which='map') # To get a visual
 pa <- locator(type='l')
 # Now select your points on the map in the Atlantic
-a <- subset(s, polygon=list(longtide=pa$x, latitude=pa$y))
+a <- subset(s, polygon=list(longitude=pa$x, latitude=pa$y))
 points(a[['longitude']], a[['latitude']], col=2)
 pp <- locator(type='l')
 # Now select your points on the map in the Pacific
-p <- subset(s, polygon=list(longtide=pp$x, latitude=pp$y))
+p <- subset(s, polygon=list(longitude=pp$x, latitude=pp$y))
 points(p[['longitude']], p[['latitude']], col=3)
