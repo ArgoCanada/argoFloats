1ba913262860c6f99f62301905894b761aced6a4
Jaimie Harbin <58750538+j-harbin@users.noreply.github.com>
2020-05-20 08:16:40 -0300

    Update polygonPlot.R

--- sandbox/jlh/08_polygonPlot/polygonPlot.R
+++ sandbox/jlh/08_polygonPlot/polygonPlot.R
@@ -2,7 +2,8 @@ par(mfrow=c(1,2))
 library(argoFloats)
 ai <- getIndex()
 s <- subset(ai, circle=list(longitude=-83, latitude=9, radius=150))
-plot(s, which='map', legend(c('Atlantic', 'Pacific'), col=c('red', 'blue'))) # To get a visual
+plot(s, which='map', legend("topright", c("Atlantic","Pacific"), col=c("red","blue"), pch=c(20,20))) # To get a visual
+legend("bottomleft", c("Atlantic","Pacific"), col=c("red","blue"), pch=c(20,20))
 pax <- c(-83.31811, -81.46290, -81.46290, -82.33111, -83.10792, -83.40950)
 pay <- c(10.365979, 10.365979,  8.876569,  9.048076,  9.698001, 10.384033)
 # Now select your points on the map in the Atlantic
@@ -17,8 +18,10 @@ points(p[['longitude']], p[['latitude']],pch=20, col='blue')
 ## Making TS Diagram for both Atlantic and Pacific
 profilesA <- getProfiles(a)
 argosA <- readProfiles(profilesA)
-plot(argosA, which='TS', col='red')
+plot(argosA, which='TS', col='red', xlim=c(30,38), ylim=c(0,30))
 par(new=TRUE)
 profilesP <- getProfiles(p)
 argosP <- readProfiles(profilesP)
-plot(argosP, which='TS', col='blue', axis(outer=FALSE))
+T <- unlist(argosP[['temperature']])
+S <- unlist(argosP[['salinity']])
+points(S,T, pch=20, col='blue')
