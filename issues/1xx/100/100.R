fbaf9c97727f90d57dfb8bbaac9d44dc447ac9cb
dankelley <kelley.dan@gmail.com>
2020-05-22 07:46:13 -0300

    issue 100 test code

---
+++ issues/1xx/100/100.R
@@ -0,0 +1,13 @@
+library(argoFloats)
+bai <- getIndex(filename='merge')
+s <- subset(bai, circle=list(longitude=-83, latitude=9, radius=800))
+plot(s, which='map') # To get a visual
+pax <- c(-92.56199, -87.32731, -83.66646, -79.35555, -88.28529, -92.76727, -92.76727, -92.73306)
+pay <- c(15.204447, 12.422940,  9.071727,  5.653489,  5.720514,  5.653489, 10.546261, 15.439032)
+a <- subset(s, polygon=list(longitude=pax, latitude=pay))
+subo <- subset(a, parameter='DOXY')
+profiles <- getProfiles(subo)
+argos <- readProfiles(profiles)
+pressure <- unlist(argos[["pressure"]])
+oxygen <- unlist(argos[["oxygen"]])
+summary(oxygen)
