b76379553be1c20b9a5266dd4cf02567df101ad3
Jaimie Harbin <jm810136@dal.ca>
2020-05-28 10:20:07 -0300

    tweaks to code

--- videos/06/video06_trajectory_by_time.R
+++ videos/06/video06_trajectory_by_time.R
@@ -12,7 +12,7 @@ lon <- index0[["longitude"]][o]
 lat <- index0[["latitude"]][o]
 profile <- index0[["profile"]][o]
 t <- index0[["date"]][o]
-if (!interactive()) png("video06.png", unit="in", width=3.5, height=3.2, pointsize=11, res=150)
+if (!interactive()) png("video06.png", unit="in", width=7, height=7, pointsize=12, res=150)
 cm <- colormap(t, col=oceColorsJet)
 par(mar=c(2,2,1,1))
 drawPalette(colormap=cm, tformat="%Y-%m", pos=4)
@@ -20,6 +20,6 @@ plot(index0, bathymetry=FALSE)
 points(lon, lat, pch=21, cex=1, bg=cm$zcol, col="darkgrey", lwd=0.5)
 points(-77.06,26.54, pch="*", cex=1.8, col='black')
 lines(lon, lat)
-sub <- seq(0, length(lon), by=10)
+sub <- seq(1, length(lon), by=10)
 text(lon[sub], lat[sub], profile[sub], cex=2/3, pos=1)
 mtext(paste("Float", ID0), adj=0)
