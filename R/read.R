34f08c67a200ffd03e5a38f12203ba5278e12ea1
dankelley <kelley.dan@gmail.com>
2020-05-29 09:00:59 -0300

    fix readProfiles() bug (close issue 111)

--- R/read.R
+++ R/read.R
@@ -65,7 +65,7 @@ readProfiles <- function(profiles, handleFlags=TRUE, silent=FALSE, debug=0)
     ## has to do with oce::read.argo() doing a require(ncdf4), which causes an error message in
     ## checking argoFloats.  But if we put argoFloats in the "Depends" field of the argoFloats
     ## DESCRIPTION file, we get an error because ArgoFloats is not using it.
-    ncversion <- nc_version()
+    ncversion <- ncdf4::nc_version()
     argoFloatsDebug(debug, "ncdf4 version: ", ncversion, "\n")
 
     res <- new("argoFloats", type="argos")
@@ -94,7 +94,7 @@ readProfiles <- function(profiles, handleFlags=TRUE, silent=FALSE, debug=0)
     }
     ## tabulate flags (ignore "Adjusted" items)
     if (!silent || debug) {
-        flagNamesAll <- unique(sort(sapply(res@data$argos, function(a) names(a@metadata$flags))))
+        flagNamesAll <- unique(sort(unlist(lapply(res@data$argos, function(a) names(a@metadata$flags)))))
         flagNames <- flagNamesAll[!grepl("Adjusted$", flagNamesAll)]
         for (flagName in flagNames) {
             percentBad <- sapply(res@data$argos,
--- sandbox/dek/08_oxygen/08_oxygen_1.R
+++ sandbox/dek/08_oxygen/08_oxygen_1.R
@@ -1,5 +1,5 @@
 library(argoFloats)
 data("indexSynthetic") # 39 items
-data("indexMerged") # 39 items
+##data("indexMerged") # 39 items
 p <- getProfiles(indexSynthetic)
 a <- readProfiles(p)
