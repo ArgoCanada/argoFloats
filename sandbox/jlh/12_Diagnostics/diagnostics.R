345b2f64d3918dc3c6f6de100fab50ec89d90645
Jaimie Harbin <jm810136@dal.ca>
2020-06-02 08:36:33 -0300

    Update diagnostics.R
    changing the word 'item' to 'variable'

--- sandbox/jlh/12_Diagnostics/diagnostics.R
+++ sandbox/jlh/12_Diagnostics/diagnostics.R
@@ -1,10 +1,10 @@
 library(argoFloats)
 library(oce)
 qf <- function(x) {
-    100 * (1 - sum(4 == x[[paste0(item, 'Flag')]]) / length(x[[paste0(item, 'Flag')]]))
+    100 * (1 - sum(4 == x[[paste0(variable, 'Flag')]]) / length(x[[paste0(variable, 'Flag')]]))
 }
 meanf <- function(x) {
-    mean(x[[item, na.rm=TRUE]])
+    mean(x[[variable, na.rm=TRUE]])
 }
 if (!exists("bai")) {
     bai <- getIndex(file='bgc')
@@ -13,22 +13,22 @@ if (!exists("bai")) {
     argos <- readProfiles(profiles, handleFlags=FALSE) 
 }
 time <- oce::numberAsPOSIXct(unlist(lapply(argos[['profile']], function(x) x[['time']])))
-items <- c('oxygen', 'salinity', 'temperature')
-for (item in items) {
+variables <- c('oxygen', 'salinity', 'temperature')
+for (variable in variables) {
     q <- unlist(lapply(argos[['profile']], qf))
     m <- unlist(lapply(argos[['profile']], meanf))
     par(mfrow=c(2,1), mar=c(2.5,2.5,1,1))
     if (any(is.finite(q))) {
-        oce.plot.ts(time,q, ylab=paste(item, "% Good"), drawTimeRange = FALSE)
+        oce.plot.ts(time,q, ylab=paste(variable, "% Good"), drawTimeRange = FALSE)
         abline(h=50, col='red', lty='dashed')
-        oce.plot.ts(time, m, ylab=paste(item, "Mean"), type='l', col='grey', drawTimeRange = FALSE)
+        oce.plot.ts(time, m, ylab=paste(variable, "Mean"), type='l', col='grey', drawTimeRange = FALSE)
         points(time, m, col=ifelse(q < 50, 'red', 'black'), pch=20, cex=0.75)
     } else {
         plot(0:1, 0:1, xlab="", ylab='', type="n", axes=FALSE)
         box()
-        text(0, 0.5, paste(' No', item, 'flags available'), pos=4)
+        text(0, 0.5, paste(' No', variable, 'flags available'), pos=4)
         plot(0:1, 0:1, xlab="", ylab='', type="n", axes=FALSE)
         box()
-        text(0, 0.5, paste(' No', item, 'flags available'), pos=4)
+        text(0, 0.5, paste(' No', variable, 'flags available'), pos=4)
     }
 }
