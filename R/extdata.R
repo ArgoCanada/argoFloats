2cafd33cbd6582b9f73e61a3f86939e1e5c6a6cd
dankelley <kelley.dan@gmail.com>
2020-06-08 14:54:29 -0300

    document SR2902204_131.nc.Rd

---
+++ R/extdata.R
@@ -0,0 +1,20 @@
+#' Sample argo file (synthetic realtime data)
+#'
+#' This is profile 131 for float 2902204.  As its filename indicates,
+#' this is in the "synthetic" data stream of "realtime" data.
+#'
+#' @name SR2902204_131.nc
+#'
+#' @docType data
+#'
+#' @encoding UTF-8
+#'
+#' @examples
+#' library(argoFloats)
+#' a <- readProfiles(system.file("extdata", "SR2902204_131.nc", package="argoFloats"))
+#' summary(a)
+#' summary(a[[1]])
+#'
+#' @family raw datasets
+NULL
+
