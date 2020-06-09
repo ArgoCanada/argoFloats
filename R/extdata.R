0d7d38f9208c8e037bb2ddf96f49c44578bc308d
dankelley <kelley.dan@gmail.com>
2020-06-09 08:28:03 -0300

    add test of reading with new .nc file

--- R/extdata.R
+++ R/extdata.R
@@ -1,7 +1,8 @@
 #' Sample argo file (synthetic realtime data)
 #'
-#' This is profile 131 for float 2902204.  As its filename indicates,
-#' this is in the "synthetic" data stream of "realtime" data.
+#' This is profile 131 for float 2902204, downloaded on June 8,
+#' 2020.  As its filename indicates, this is in the "synthetic"
+#' data stream of "realtime" data.
 #'
 #' @name SR2902204_131.nc
 #'
