da215b890ec4d40f93d3c6b485ea16795cbec1ca
dankelley <kelley.dan@gmail.com>
2020-06-09 14:21:38 -0300

    .

--- sandbox/dek/09_adjust/09_adjust_02.R
+++ sandbox/dek/09_adjust/09_adjust_02.R
@@ -7,24 +7,36 @@ p <- getProfiles(s)
 a <- readProfiles(p, adjusted=TRUE)
 cat("Demonstration: adjusted data are not always good!\n")
 testOfHypothesis <- NULL
-for (argo in a[["profile"]]) {
+for (i in seq_len(a[["length"]])) {
+    argo <- a[[i]]
     isRealtime <- grepl("/[SM]{0,1}R", argo[["filename"]])
     allNA <- all(!is.finite(argo[["temperature"]]))
     hypothesis <- if (isRealtime) allNA else TRUE
     testOfHypothesis <- c(testOfHypothesis, hypothesis)
-    cat(argo[["filename"]], " hypothesis is ", if (hypothesis) "TRUE\n" else "FALSE\n", sep="")
-    cat("    mean temperature:           ", mean(argo[["temperature"]], na.rm=TRUE), "\n")
-    cat("    mean temperatureUnadjusted: ", mean(argo[["temperatureUnadjusted"]], na.rm=TRUE), "\n")
+    cat(sprintf("%2d %s hypothesis=%s, dataMode=%s\n",
+                i,
+                gsub(".*/data/argo/", "", argo[["filename"]]),
+                hypothesis,
+                paste(argo[["dataMode"]], collapse=",")))
+    cat(sprintf("   T: %6.3f unadjusted: %6.3f\n",
+                mean(argo[["temperature"]], na.rm=TRUE),
+                mean(argo[["temperatureUnadjusted"]], na.rm=TRUE)))
 }
-table(testOfHypothesis)
-cat("\n\nHypothesis failed on cases: ", paste(which(!testOfHypothesis), collapse=" "), "\n")
+dataMode <- unlist(lapply(a[["profile"]], function(x) x[["dataMode"]][1]))
+cat("\n\n")
+cat("Hypothesis worked on cases: ", paste(which(testOfHypothesis), collapse=" "), "\n", sep="")
+cat("datamode=='D'     on cases: ", paste(which(dataMode=="D"), collapse=" "), "\n", sep="")
+cat("Hypothesis failed on cases: ", paste(which(!testOfHypothesis), collapse=" "), "\n", sep="")
+cat("datamode=='A'     on cases: ", paste(which(dataMode=="A"), collapse=" "), "\n", sep="")
 
 bad <- which(!testOfHypothesis)
 good <- which(testOfHypothesis)
 
+badExample <- a[[bad[1]]]
 cat("\n\nFirst failed test is case #", bad[1], ", which has following summary:\n", sep="")
-summary(a[[bad[1]]])
+summary(badExample)
 
+goodExample <- a[[good[1]]]
 cat("\n\nFirst good test is case #", good[1], ", which has following summary:\n", sep="")
-summary(a[[good[1]]])
+summary(goodExample)
 
