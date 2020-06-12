851a3983b6a011ea7a3456eda762b40aae310769
dankelley <kelley.dan@gmail.com>
2020-06-12 14:29:59 -0300

    add D4900785_048.nc dataset

--- R/extdata.R
+++ R/extdata.R
@@ -19,3 +19,23 @@
 #' @family raw datasets
 NULL
 
+#' Sample argo file (delayed data)
+#'
+#' This is profile 048 for float 4900785, downloaded on June 12,
+#' 2020.  As its filename indicates, it holds "realtime" data.
+#'
+#' @name D4900785_048.nc 
+#'
+#' @docType data
+#'
+#' @encoding UTF-8
+#'
+#' @examples
+#' library(argoFloats)
+#' a <- readProfiles(system.file("extdata", "D4900785_048.nc", package="argoFloats"))
+#' summary(a)
+#' summary(a[[1]])
+#'
+#' @family raw datasets
+NULL
+
--- create_data/create_profiles.R
+++ create_data/create_profiles.R
@@ -1,6 +1,9 @@
 library(argoFloats)
 
-if (TRUE) {
+## these blocks are FALSE because we do not want the resultant files to be
+## changed if the code is rerun later.
+
+if (FALSE) {
     message("No need to download the index and select the file, because already done")
     ## ~/data/argo/SR2902204_131.nc
     ## https://github.com/ArgoCanada/argoFloats/issues/117#issuecomment-640758567
@@ -11,5 +14,22 @@ if (TRUE) {
     stopifnot(s3[['file']] == "incois/2902204/profiles/SR2902204_131.nc")
     system("cp ~/data/argo/SR2902204_131.nc .") # retain for a backup
     system("cp ~/data/argo/SR2902204_131.nc ../inst/extdata") # official location
+}
+
+if (FALSE) {
+    ## First profile of most frequent delayed-mode case in data(index)
+    library(argoFloats)
+    data(index)
+    s1 <- subset(index, mode="delayed")
+    t <- table(s1[["ID"]])
+    ID <- names(t[which.max(t)])           # 4900785
+    s2 <- subset(s1, ID=ID)
+    profile <- s2[["profile"]][1]
+    s3 <- subset(s2, profile=profile)
+    stopifnot(s3[["file"]] == "aoml/4900785/profiles/D4900785_048.nc")
+    p <- getProfiles(s3)
+    file <- p[["file"]]
+    system(paste("cp", file, "."))
+    system(paste("cp", file, "../inst/extdata")) # official location
 }
 
