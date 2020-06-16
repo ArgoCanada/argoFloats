5356e8d8b3b0cbd46d128eeeac1f13cb9cdcf4fc
Jaimie Harbin <jm810136@dal.ca>
2020-06-16 07:50:34 -0300

    Update qcFunction.R
    - Still problematic if HISTORY_QCTEST is not in the same order as before
    ( Error in a[["HISTORY_QCTEST"]] : subscript out of bounds)

--- sandbox/jlh/17_qcFunction/qcFunction.R
+++ sandbox/jlh/17_qcFunction/qcFunction.R
@@ -1,3 +1,9 @@
+library(argoFloats)
+data('index')
+s <- subset(index,953)
+gp <- getProfiles(s)
+rp <- readProfiles(gp)
+
 qf <- function(a) {
     if(!inherits(a, 'argos'))
         ("Can only provide Quality Control Summary for a single argo profile")
@@ -7,9 +13,7 @@ qf <- function(a) {
     failed <- which(action == 'QCF$')
     B <- function(x) ifelse(tail(rev(rawToBits(as.raw(paste0('0x0',x)))),4) == '01', 1, 0)
     BB <- function(x) unlist(lapply(strsplit(x, '')[[1]], function(l) B(l)))
-    #cat("Tests performed:", paste(which(1 == tail(rev(BB('QCP$')),-1)), collapse=' ')) # make these general
-    #cat("Tests failed:", paste(which(1 == tail(rev(BB('QCF$')),-1)), collapse=' ')) # make these general
-    
-    cat("Tests performed:", paste(which(1 == tail(rev(BB('5f03e')),-1)), collapse=' ')) # make these general
-    cat("Tests failed:", paste(which(1 == tail(rev(BB('4000')),-1)), collapse=' ')) # make these general
+    {cat("Tests performed:", paste(which(1 == tail(rev(BB('QCP$')),-1)), collapse=' '), "\n")
+        cat("Tests failed:", paste(which(1 == tail(rev(BB('QCF$')),-1)), collapse=' '))
+    }
 }
