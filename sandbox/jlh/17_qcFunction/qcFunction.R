9302fcee8eacc46f221d8585c8755098468b3188
Jaimie Harbin <jm810136@dal.ca>
2020-06-16 11:28:30 -0300

    changes

--- sandbox/jlh/17_qcFunction/qcFunction.R
+++ sandbox/jlh/17_qcFunction/qcFunction.R
@@ -1,3 +1,4 @@
+options(warn=1)
 library(argoFloats)
 data('index')
 s <- subset(index,100)
@@ -7,18 +8,33 @@ rp1 <- rp[[1]]
 rp1[['HISTORY_QCTEST']]
 rp1[['HISTORY_ACTION']]
 
-qf <- function(a) {
-    if(!inherits(a, 'argos'))
-        ("Can only provide Quality Control Summary for a single argo profile")
+showQCTests <- function(a) {
+    if (!inherits(a, 'argo'))
+        stop("Can only provide Quality Control Summary for argo object")
     result <-a[['HISTORY_QCTEST']]
     action <- a[['HISTORY_ACTION']]
     performed <- which(action == 'QCP$')
+    cat(vectorShow(result))
     failed <- which(action == 'QCF$')
-    B <- function(x) ifelse(tail(rev(rawToBits(as.raw(paste0('0x0',x)))),4) == '01', 1, 0)
-    BB <- function(x) unlist(lapply(strsplit(x, '')[[1]], function(l) B(l)))
-    {cat("Tests performed:", paste(which(1 == tail(rev(BB('QCP$')),-1)), collapse=' '), "\n")
-        cat("Tests failed:", paste(which(1 == tail(rev(BB('QCF$')),-1)), collapse=' '))
+    cat(vectorShow(result[,2]))
+    cat(vectorShow(failed))
+    # x is a hex string
+    B <- function(x) {
+        cat(vectorShow(x))
+        tmp2 <- paste0('0x0',x)
+        cat(vectorShow(tmp2))
+        res <- ifelse(tail(rev(rawToBits(as.raw(paste0('0x0',x)))),4) == '01', 1, 0)
+        cat(vectorShow(res))
+        res
     }
+    # 
+    BB <- function(x) {
+        cat("Next is x to BB \n")
+        print(x)
+        unlist(lapply(strsplit(x, '')[[1]], function(l) B(l)))
+    }
+    cat("Tests performed:", paste(which(1 == tail(rev(BB(performed)),-1)), collapse=' '), "\n")
+    #cat("Tests failed:   ", paste(which(1 == tail(rev(BB(failed)),-1)), collapse=' '), "\n")
 }
 
-qf('rp1')
+showQCTests(rp1)
