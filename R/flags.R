2af3e254e8c444649f3749c56e54c01cca9addd7
dankelley <kelley.dan@gmail.com>
2020-06-18 07:38:38 -0300

    useFlags() chosen as (temporary?) name

--- R/flags.R
+++ R/flags.R
@@ -1,95 +1,61 @@
 ## vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
 
-## ## @import oce
-## #' @importFrom oce presentTime
-## setGeneric("handleFlags", getGeneric("handleFlags", package="oce"))
-
-
-#' Handle flags in an argoFloats object of type "argos"
+#' Use quality-control flags to alter argoFloats data
 #'
-#' The work is done by using [oce::handleFlags,argo-method()] on each of the
-#' argo profiles that are stored in the `argos` item within the `data` slot
-#' of `object`.  Apart from `object`, all arguments are passed to
-#' [oce::handleFlags,argo-method()].
+#' This function examines the quality-control (QC) flags within an [argoFloats-class]
+#' object that was created by [readProfiles()].  By default, it replaces all suspicious
+#' data with `NA` values, so they will not appear in plots or be considered in calculations.
+#' This is an important early step in processing, because suspicious argo floats commonly
+#' report data that are suspicious based on statistical and physical measures, as
+#' is illustrated in the \dQuote{Examples} section.
 #'
-#' @param object A [argoFloats-class] object of type `"argos"`, as created by
+#' The work is done by using [oce::handleFlags,argo-method()]
+#' on each of the profiles storedwithin the object. In most cases, only
+#' the object needs to be specified, for the default actions coincide with
+#' common conventions for flags in argo data.
+#'
+#' @param x An [argoFloats-class] object of type `"argos"`, as created by
 #' [readProfiles()].
 #'
-#' @param flags A vector of integers that are flag values for data that are
-#' to be set to `NA` in the returned result.  The default, `NULL`, means to
-#' use the flags set up by [readProfiles()].
+#' @param flags A vector of integers that are the quality-control flag
+#' values for data that are to be set to `NA` in the returned result.
+#' The default, `NULL`, means to use the flags set up by [readProfiles()].
+#' That means to regard as suspicious any data entries that are flagged
+#' with QC codes of 0 (for data that have not yet been assessed),
+#' 3 (for "probably bad" data),
+#' 4 (for "bad" data), or
+#' 9 (for "missing" data).
 #'
 #' @param actions The actions to perform. The default, `NULL`, means to
-#' use the actions set up by [readProfiles()].
-#'
-#' @param where Passed to [oce::handleFlags,argo-method()].
+#' use the actions set up by [readProfiles()], which, by default.
+#' causes any data flagged as suspicious to be set to `NA`.
 #'
 #' @param debug Passed to [oce::handleFlags,argo-method()].
 #'
-#' @return A copy of `object` but with each argo object contained therein having
-#' been passed through [oce::handleFlags,argo-method()].
-#'
-#' @export
+#' @return A copy of `x` but with each of the objects within its
+#' `data` slot having been passed through [oce::handleFlags,argo-method()].
 #'
 #' @examples
 #' library(argoFloats)
 #' # Contrast TS diagrams for raw and flag-handled data
 #' data(index)
 #' i <- subset(index, 1:5) # first 5 profiles
-#' a <- readProfiles(getProfiles(i))
-#' A <- handleFlags(a)
+#' raw <- readProfiles(getProfiles(i))
+#' clean <- useFlags(raw)
 #' par(mfrow=c(1, 2))
-#' plot(a, which="TS")
-#' plot(A, which="TS")
-#'
-#' @author Dan Kelley
-setGeneric("handleFlags", function(object, flags=NULL, actions=NULL, where=NULL, debug=getOption("oceDebug")) {
-           standardGeneric("handleFlags")
-         })
-
-#' Handle flags in an argoFloats object of type "argos"
-#'
-#' The work is done by using [oce::handleFlags,argo-method()] on each of the
-#' argo profiles that are stored in the `argos` item within the `data` slot
-#' of `object`.  Apart from `object`, all arguments are passed to
-#' [oce::handleFlags,argo-method()].
-#'
-#' @param object A [argoFloats-class] object of type `"argos"`, as created by
-#' [readProfiles()].
-#'
-#' @param flags A vector of integers that are flag values for data that are
-#' to be set to `NA` in the returned result.  The default, `NULL`, means to
-#' use the flags set up by [readProfiles()].
-#'
-#' @param actions The actions to perform. The default, `NULL`, means to
-#' use the actions set up by [readProfiles()].
-#'
-#' @param where Passed to [oce::handleFlags,argo-method()].
-#'
-#' @param debug Passed to [oce::handleFlags,argo-method()].
-#'
-#' @return A copy of `object` but with each argo object contained therein having
-#' been passed through [oce::handleFlags,argo-method()].
+#' plot(raw, which="TS")
+#' plot(clean, which="TS")
 #'
 #' @export
-#'
-#' @examples
-#' library(argoFloats)
-#' # Contrast TS diagrams for raw and flag-handled data
-#' data(index)
-#' i <- subset(index, 1:5) # first 5 profiles
-#' a <- readProfiles(getProfiles(i))
-#' A <- handleFlags(a)
-#' par(mfrow=c(1, 2))
-#' plot(a, which="TS")
-#' plot(A, which="TS")
-#'
 #' @author Dan Kelley
-setMethod("handleFlags", signature=c(object="argoFloats", flags="ANY", actions="ANY", where="ANY", debug="ANY"),
-          definition=function(object, flags=NULL, actions=NULL, where=NULL, debug=0) {
-              if ("argos" != object[["type"]])
-                  stop("can only handle flags in an object of type \"argos\", as created with readProfiles()")
-              res <- object
-              res@data$argos <- lapply(object@data$argos, oce::handleFlags, flags=flags, actions=actions, where=NULL, debug=debug)
-              res
-          })
+useFlags <- function(x, flags=NULL, actions=NULL, debug=0)
+{
+    if (!requireNamespace("oce", quietly=TRUE))
+        stop("must install.packages(\"oce\") for useFlags() to work")
+    if ("argos" != x[["type"]])
+        stop("can only handle flags in an object of type \"argos\", as created with readProfiles()")
+    res <- x
+    res@data$argos <- lapply(x@data$argos, oce::handleFlags, flags=flags, actions=actions, debug=debug)
+    res@processingLog <- oce::processingLogAppend(x@processingLog, paste(deparse(match.call()), sep="", collapse=""))
+    res
+}
