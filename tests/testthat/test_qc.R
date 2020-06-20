bb7c5e4a5ed53b8bea19388fa96f6d8bbf57950a
dankelley <kelley.dan@gmail.com>
2020-06-20 14:01:23 -0300

    tests cannot download (for qc)

--- tests/testthat/test_qc.R
+++ tests/testthat/test_qc.R
@@ -1,18 +1,20 @@
 ## vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
 library(argoFloats)
+source("can_download.R")
 context("processing of QC flags")
 
 test_that("applyQC with default 'flag' and 'action' arguments",
           {
-              skip_on_cran()
-              data(index)
-              i <- subset(index, 1:5) # first 5 profiles
-              raw <- expect_warning(readProfiles(getProfiles(i)), "^Of 5 profiles read")
-              clean <- applyQC(raw)
-              for (i in raw[["length"]]) {
-                  for (field in c("temperature", "salinity", "pressure")) {
-                      bad <- raw[["profile", i]][[paste0(field, "Flag")]] %in% c(0,3,4,6,7,9)
-                      expect_true(all(is.na(clean[["profile", i]][[field]]) == bad))
+              if (canDownload()) {
+                  data(index)
+                  i <- subset(index, 1:5) # first 5 profiles
+                  raw <- expect_warning(readProfiles(getProfiles(i)), "^Of 5 profiles read")
+                  clean <- applyQC(raw)
+                  for (i in raw[["length"]]) {
+                      for (field in c("temperature", "salinity", "pressure")) {
+                          bad <- raw[["profile", i]][[paste0(field, "Flag")]] %in% c(0,3,4,6,7,9)
+                          expect_true(all(is.na(clean[["profile", i]][[field]]) == bad))
+                      }
                   }
               }
           }
