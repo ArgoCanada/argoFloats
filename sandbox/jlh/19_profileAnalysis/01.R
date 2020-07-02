2268bb9aeae20eee044386be456d631ac05bfd18
Jaimie Harbin <jm810136@dal.ca>
2020-07-02 07:04:37 -0300

    .

--- sandbox/jlh/19_profileAnalysis/01.R
+++ sandbox/jlh/19_profileAnalysis/01.R
@@ -1,7 +1,31 @@
 library(argoFloats)
-ai <- getIndex()
-from <- as.POSIXct("2020-01-01", tz="UTC")
-to <- as.POSIXct("2020-01-31", tz="UTC")
-sub <- subset(ai, time=list(from=from, to=to))
-s4 <- subset(sub, 1:4)
-s4[['file']]
+library(oce)
+## https://argovis.colorado.edu/selection/profiles/1/2020
+library(rjson)
+base <- "https://argovis.colorado.edu/selection/profiles"
+month <- 1
+year <- 2020
+source <- paste0(base, "/", month, "/", year)
+file <- paste0(month, "_", year, ".json")
+if (!file.exists(file)) download.file(source, file) else cat("Using pre-downloaded file '", file, "'\n", sep="")
+a <- fromJSON(file=file)
+ids <- sapply(a, function(x) x[["_id"]])
+modes <- sapply(a, function(x) x[["DATA_MODE"]])
+lons <- sapply(a, function(x) x[["lon"]])
+lats <- sapply(a, function(x) x[["lat"]])
+params <- sapply(a, function(x) paste0(x[["station_parameters"]], collapse=","))
+dacs <- sapply(a, function(x) paste0(x[["dac"]], collapse=","))
+times <- oce::numberAsPOSIXct(sapply(a, function(x) as.POSIXct(gsub("Z$", "", x[["date"]]), "%Y-%m-%dT%H:%M:%S", tz="UTC")))
+deeps <- sapply(a, function(x) paste0(x[["isDeep"]], collapse=","))
+BGCs <- sapply(a, function(x) paste0(x[["containsBGC"]], collapse=","))
+n <- 20
+cat("Some elements of the first", n, "profiles are:\n")
+options(width=150)
+df <- data.frame(ID=ids, mode=modes, deep=deeps, longitude=lons, latitude=lats, dac=dacs, time=times, params=params, BGC=BGCs)
+print(head(df, n))
+stop()
+head(df, 4)
+class(params) # character
+params[[1]]
+parameters <- strsplit(params, split= " ")
+
