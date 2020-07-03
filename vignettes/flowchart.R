4daf6749f0dc3bc06fbb98943c90a5afe2c02c14
dankelley <kelley.dan@gmail.com>
2020-07-03 14:19:55 -0300

    flowcharts made more similar; wording expanded

--- vignettes/flowchart.R
+++ vignettes/flowchart.R
@@ -1,12 +1,12 @@
-if (!interactive()) png("flowchart.png", unit="in", width=5, height=2.8, pointsize=11, res=150)
+if (!interactive()) png("flowchart.png", unit="in", width=5, height=2.8, pointsize=10, res=120)
 
 colText <- "darkblue"
 colCode <- "black"
 library(graphics)
 textInBox <- function(x, y, text, cex=1, pos=4, center=TRUE, family="Times New Roman", col="black", tweakx=0)
 {
-    w <- graphics::strwidth(text)
-    h <- graphics::strheight(text)
+    w <- graphics::strwidth(text, family=family)
+    h <- graphics::strheight(text, family=family)
     if (center)
         x <- x - w/2
     text(x+tweakx, y, text, cex=cex, pos=pos, family=family, col=col)
@@ -19,38 +19,37 @@ plot(c(0, 1), c(0, 1), type="n", xlab="", ylab="", axes=FALSE)
 x0 <- 0.25
 y0 <- 0.9
 dy <- 0.2
-wh <- textInBox(x0, y0, "Get index from server", col=colText)
+wh <- textInBox(x0, y0, "Get an index of profiles from an argo server.", col=colText)
 h <- wh$h * 1.2
 xarrow <- x0 + 0.01 # not sure why we need to move it
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1, col=colText)
 y0 <- y0 - dy
-textInBox(x0, y0, "Focus on a subset of profiles", col=colText)
+textInBox(x0, y0, "Focus on a subset of profiles.", col=colText)
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1, col=colText)
 y0 <- y0 - dy
-textInBox(x0, y0, "Get profile data files from server", col=colText)
+textInBox(x0, y0, "Get profile data files from the server.", col=colText)
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1, col=colText)
 y0 <- y0 - dy
-textInBox(x0, y0, "Read profile data files", col=colText)
+textInBox(x0, y0, " Read profile data files. ", col=colText)
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1, col=colText)
 y0 <- y0 - dy
-textInBox(x0, y0, "Process the data", col=colText)
+textInBox(x0, y0, " Process the data.  ", col=colText)
 x0 <- 0.8
 y0 <- 0.9
-wh <- textInBox(x0, y0, "getIndex() ", family="sans", col=colCode)
+wh <- textInBox(x0, y0, "getIndex()  ", family="sans", col=colCode)
 h <- wh$h * 1.2
 xarrow <- x0 + 0.01 # not sure why we need to move it
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1)
 y0 <- y0 - dy
-textInBox(x0, y0, "subset() ", family="sans", col=colCode)
+textInBox(x0, y0, "subset()  ", family="sans", col=colCode)
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1)
 y0 <- y0 - dy
-textInBox(x0, y0, "getProfiles() ", family="sans", col=colCode)
+textInBox(x0, y0, "getProfiles()  ", family="sans", col=colCode)
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1)
 y0 <- y0 - dy
-textInBox(x0, y0, "readProfiles() ", family="sans", col=colCode)
+textInBox(x0, y0, "readProfiles()  ", family="sans", col=colCode)
 arrows(xarrow, y0-h, xarrow, y0-dy+h, length=0.1)
 y0 <- y0 - dy
 textInBox(x0, y0, ". . .   ", family="sans", col=colCode, tweakx=0.005)
 par(mar=omar)
 if (!interactive()) dev.off()
-```
