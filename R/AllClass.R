b23a4de0a86025a46751b808bfe7c5a3ec295804
dankelley <kelley.dan@gmail.com>
2020-07-21 10:31:18 -0300

    cleanup

--- R/AllClass.R
+++ R/AllClass.R
@@ -334,8 +334,7 @@ setMethod(f="[[",
                       return(if (missing(j)) x@data$index[[i]] else x@data$index[[i]][j])
                   } else if (length(i) == 1 && i == "cycle") {
                       cycle <- gsub("^[a-z]*/[0-9]*/profiles/[A-Z]*[0-9]*_([0-9]{3,4}[D]{0,1}).nc$", "\\1", x@data$index$file)
-                      Jaimie <<- profile
-                      return(as.vector(if (missing(j)) profile else profile[j]))
+                      return(as.vector(if (missing(j)) cycle else cycle[j]))
                   } else if (length(i) == 1 && i == "ID") {
                       ID <- gsub("^[a-z]*/([0-9]*)/profiles/[A-Z]*[0-9]*_[0-9]{3}[A-Z]*.nc$", "\\1", x@data$index$file)
                       return(as.vector(if (missing(j)) ID else ID[j]))
