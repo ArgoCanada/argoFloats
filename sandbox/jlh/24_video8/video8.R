e027bad4251ae7930eb1729c86905fce4e31de37
Jaimie Harbin <jm810136@dal.ca>
2020-08-11 12:36:31 -0300

    Update video8.R
    identifying to show Dan

--- sandbox/jlh/24_video8/video8.R
+++ sandbox/jlh/24_video8/video8.R
@@ -11,10 +11,39 @@ index1[['cycle']]
 index2 <- subset(index1, cycle='124') 
 profiles2 <- getProfiles(index2)
 argos2 <- readProfiles(profiles2)
-showQCTests(argos2[[1]])
+showQCTests(argos2[[1]], style='full')
 
 # applyQC()
 clean <- applyQC(argos)
 par(mfrow=c(1, 2))
 plot(argos, which="TS")
 plot(clean, which="TS")
+
+# Exploring bad Data
+# left point
+par(mfrow=c(1, 1))
+T <- unlist(clean[['temperature']])
+S <- unlist(clean[['salinity']])
+plot(S,T)
+identify(S,T)
+# To determine which profile the 467th position is in
+for (x in 1:9){
+    print(length(unlist(clean[['salinity']][x])))
+}
+# This tells us is in the 7th cycle at position 54.
+S[467]
+argos[['salinity']][[7]][54,]
+T[467]
+argos[['temperature']][[7]][54,]
+salinityF <- unlist(clean[['salinityFlag']])[467]
+temperatureF <- unlist(clean[['temperatureFlag']])[467]
+
+# Right points
+identity(S,T)
+salinityFR <- unlist(clean[['salinityFlag']])[546]
+temperatureFR <- unlist(clean[['temperatureFlag']])[546]
+
+
+
+
+
