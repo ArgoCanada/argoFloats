460dfd6942406c921c22c13889ba8d2eb7546a87
Jaimie Harbin <jm810136@dal.ca>
2020-08-14 09:44:22 -0300

    Update cycleParameter.R

--- sandbox/jlh/27_cycleParameter/cycleParameter.R
+++ sandbox/jlh/27_cycleParameter/cycleParameter.R
@@ -7,3 +7,16 @@ argos <- readProfiles(profiles)
 clean <- applyQC(argos)
 plot(clean, which="TS")
 
+salinity <- unlist(x[["salinity", debug=debug]])
+temperature <- unlist(x[["temperature", debug=debug]])
+pressure <- unlist(x[["pressure", debug=debug]])
+## Use byLevel to repeat the latitude and longitude values across
+## the depths in each profile, so that the resultant vector
+## will match the salinity, temperature and pressure vectors.
+latitude <- unlist(x[["latitude", "byLevel", debug=debug]])
+longitude <- unlist(x[["longitude", "byLevel", debug=debug]])
+ctd <- oce::as.ctd(salinity=salinity,
+                   temperature=temperature,
+                   pressure=pressure,
+                   latitude=latitude,
+                   longitude=longitude)
