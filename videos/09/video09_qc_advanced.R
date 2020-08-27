6c881abe9741abe48a5c4fe279c336350a84e0e3
dankelley <kelley.dan@gmail.com>
2020-08-27 10:29:56 -0300

    dog

--- videos/09/video09_qc_advanced.R
+++ videos/09/video09_qc_advanced.R
@@ -2,7 +2,7 @@
 
 ## Part 1: Overview (from video 08)
 
-# QC Plot
+## QC Plot
 library(argoFloats)
 data("index")
 index1 <- subset(index, id='1901584')
@@ -10,15 +10,15 @@ profiles <- getProfiles(index1)
 argos <- readProfiles(profiles)
 plot(argos, which='QC', parameter='temperature')
 
-# showQCTests
+## showQCTests
 index1[['cycle']]
 index2 <- subset(index1, cycle='124')
 profiles2 <- getProfiles(index2)
 argos2 <- readProfiles(profiles2)
 showQCTests(argos2[[1]])
-#showQCTests(argos2[[1]], style='full')
+##showQCTests(argos2[[1]], style='full')
 
-# applyQC()
+## applyQC()
 clean <- applyQC(argos)
 par(mfrow=c(1, 2))
 plot(argos, which="TS")
@@ -27,7 +27,7 @@ plot(clean, which="TS")
 
 ## Part 2: Exploring bad Data
 
-# Identify the bad data
+## Identify the bad data
 par(mfrow=c(1,1))
 plot(clean, which="TS", eos="gsw")
 SA <- unlist(clean[["SA"]])
@@ -37,16 +37,21 @@ SAbad <- SA[bad]
 CTbad <- CT[bad]
 points(SAbad, CTbad) # plot it, to check
 
-# Find the cycle that holds the identified bad data
+## Find the cycle that holds the identified bad data
 cycles <- clean[["argos"]]
 for (icycle in seq_along(cycles)) {
-    cycle <- cycles[[icycle]]
-    w <- which(SAbad == cycle[["SA"]] & CTbad == cycle[["CT"]])
+    thisCycle <- cycles[[icycle]]
+    w <- which(SAbad == thisCycle[["SA"]] & CTbad == thisCycle[["CT"]])
     if (length(w)) {
-        message("found bad point SA=", SAbad, ", CT=", CTbad,
-		icycle, "-th cycle (i.e. cycle = ",cycle,")",
-		" at pressure ", cycle[["pressure"]][w])
+        message(sprintf("Bad point: SA=%.3f, CT=%.3f in the %d-th cycle (i.e. cycle '%s') at %.1f dbar",
+                        SAbad, CTbad, icycle,
+                        thisCycle[["cycleNumber"]], thisCycle[["pressure"]][w]))
         break
     }
 }
+
+## Further analysis of the bad point
+clean147 <- subset(clean, cycle='147')
+plot(clean147, which="TS")
+showQCTests(clean147[[1]], style="full")
 
