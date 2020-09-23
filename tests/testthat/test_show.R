03710c6cc845bd609a72d422e40bcc4a32bba21c
Jaimie Harbin <jm810136@dal.ca>
2020-09-23 07:15:07 -0300

    Update test_show.R

--- tests/testthat/test_show.R
+++ tests/testthat/test_show.R
@@ -7,13 +7,13 @@ library(testthat)
 context("show")
 
 test_that("show", {
-          if (canDownload()){
-          data("index")
-          index1 <- expect_message(subset(index,1:5,"Kept 5 profiles (0.511%)"))
-          profiles <- getProfiles(index1)
-          expect_output(print(profiles), "argoFloats object of type \"profiles\" with 5 items")
-          expect_output(print(index), "argoFloats object of type \"index\" with 978 items")
-          argos <- expect_output(expect_warning(readProfiles(profiles),
-                                                "Of 5 profiles read, 5 have"), "|===")
-}})
+    if (canDownload()){
+        data("index")
+        index1 <- expect_message(subset(index,1:5,"Kept 5 profiles (0.511%)"))
+        profiles <- getProfiles(index1)
+        expect_output(print(profiles), "argoFloats object of type \"profiles\" with 5 items")
+        expect_output(print(index), "argoFloats object of type \"index\" with 978 items")
+        argos <- expect_output(expect_warning(readProfiles(profiles),
+                                              "Of 5 profiles read, 5 have"), "|===")
+    }})
 
