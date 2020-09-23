993c44b3bf5488fdd04e92753b813ae29a4a6363
Jaimie Harbin <jm810136@dal.ca>
2020-09-23 10:36:45 -0300

    test_show; test_accessors

--- tests/testthat/test_show.R
+++ tests/testthat/test_show.R
@@ -9,6 +9,8 @@ context("show")
 test_that("show", {
         data("index")
         index1 <- expect_message(subset(index,1:5,"Kept 5 profiles (0.511%)"))
+        expect_output(print(index), "argoFloats object of type \"index\" with 978 items")
         a <- expect_warning(readProfiles(system.file("extdata", "SR2902204_131.nc", package="argoFloats")))
+        expect_output(print(a), "argoFloats object of type \"argos\" with 1 items")
     })
 
