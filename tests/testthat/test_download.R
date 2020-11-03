07ace9002437300a9fd7e3e7c116d342b8335b68
Jaimie Harbin <jm810136@dal.ca>
2020-11-03 15:19:03 -0400

    issue274

--- tests/testthat/test_download.R
+++ tests/testthat/test_download.R
@@ -2,30 +2,31 @@
 library(argoFloats)
 source("can_download.R")
 context("bgc download")
-destdir <- "~/data/argo" # QUESTION: could we use a tmpdir and still be within CRAN guidelines?
+#destdir <- "~/data/argo" # QUESTION: could we use a tmpdir and still be within CRAN guidelines?
 
 test_that("getIndex()",
           {
-              if (canDownload()) {
-                  i <- expect_silent(getIndex(filename="argo_bio-profile_index.txt.gz", destdir=destdir))
-                  i2 <- expect_error(getIndex(filename="dog","filename=\"dog\" doesn't exist. Try one of these: \"argo\", \"core\", \"bgc\", \"bgcargo\", or \"synthetic\"."))
-                  }
+              tmp_file <- tempfile()
+              dir.create(tmp_file)
+              i <- expect_silent(getIndex(filename="argo_bio-profile_index.txt.gz", destdir=tmp_file))
+              i2 <- expect_error(getIndex(filename="dog","filename=\"dog\" doesn't exist. Try one of these: \"argo\", \"core\", \"bgc\", \"bgcargo\", or \"synthetic\"."))
+              unlink(tmp_file, recursive = TRUE)
           }
 )
 
 test_that("getProfiles()",
           {
-              if (canDownload()) {
-                  data(index)
-                  s <- expect_message(subset(index, 1:3), "Kept 3 profiles \\(0.307%\\)")
-                  p <- expect_silent(getProfiles(s))
-                  expect_equal(p[["cycle"]], c("124", "125", "126"))
-                  expect_equal(p[["file", 1]], "~/data/argo/R1901584_124.nc")
-                  expect_equal(p[["file"]], c("~/data/argo/R1901584_124.nc", "~/data/argo/R1901584_125.nc",
-                                              "~/data/argo/R1901584_126.nc"))
-                  expect_equal(p[["file", 1:2]], c("~/data/argo/R1901584_124.nc", "~/data/argo/R1901584_125.nc"))
-                  expect_equal(p[["length"]], 3)
-              }
+              tmp_file <- tempfile()
+              dir.create(tmp_file)
+              data(index)
+              s <- expect_message(subset(index, 1:3), "Kept 3 profiles \\(0.307%\\)")
+              p <- expect_silent(getProfiles(s, destdir=tmp_file))
+              expect_equal(p[["cycle"]], c("124", "125", "126"))
+              expect_equal(p[["url", 1]], "ftp://usgodae.org/pub/outgoing/argo/dac/aoml/1901584/profiles/R1901584_124.nc")
+              expect_equal(p[["url"]], c("ftp://usgodae.org/pub/outgoing/argo/dac/aoml/1901584/profiles/R1901584_124.nc","ftp://usgodae.org/pub/outgoing/argo/dac/aoml/1901584/profiles/R1901584_125.nc","ftp://usgodae.org/pub/outgoing/argo/dac/aoml/1901584/profiles/R1901584_126.nc"))
+              expect_equal(p[["url", 1:2]], c("ftp://usgodae.org/pub/outgoing/argo/dac/aoml/1901584/profiles/R1901584_124.nc","ftp://usgodae.org/pub/outgoing/argo/dac/aoml/1901584/profiles/R1901584_125.nc"))
+              expect_equal(p[["length"]], 3)
+              unlink(tmp_file, recursive = TRUE)
           }
 )
 
