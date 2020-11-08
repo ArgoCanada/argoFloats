984701062d6f84b3ea850ff9690581a97de59368
dankelley <kelley.dan@gmail.com>
2020-11-08 07:54:02 -0400

    rebuild vignette

--- vignettes/qc.R
+++ vignettes/qc.R
@@ -4,30 +4,30 @@ knitr::opts_chunk$set(collapse=TRUE, comment="#>")
 ## ----eval=FALSE---------------------------------------------------------------
 #  library(argoFloats)
 #  load_all()
-#  browseVignettes('argoFloats')
+#  browseVignettes("argoFloats")
 
 ## ----echo=FALSE---------------------------------------------------------------
-ID <- '1901584'
+ID <- "1901584"
 
 ## ---- warning=FALSE, error=FALSE, message=FALSE, eval=FALSE-------------------
 #  library(argoFloats)
 #  data("index")
-#  index1 <- subset(index, ID='1901584')
+#  index1 <- subset(index, ID="1901584")
 #  profiles <- getProfiles(index1)
 #  argos <- readProfiles(profiles)
-#  plot(argos, which='QC', parameter='temperature')
+#  plot(argos, which="QC", parameter="temperature")
 
 ## ---- message=FALSE, error=FALSE, warning=FALSE, eval=FALSE-------------------
-#  index2 <- subset(index1, cycle='124')
+#  index2 <- subset(index1, cycle="124")
 #  profiles2 <- getProfiles(index2)
 #  argos2 <- readProfiles(profiles2)
 #  showQCTests(argos2[[1]])
 
 ## ---- eval=FALSE--------------------------------------------------------------
 #  A <- argos[[1]]
-#  D <- data.frame(T=A[['temperature']], TF=A[['temperatureFlag']],
-#                  S=A[['salinity']], SF=A[['salinityFlag']],
-#                  P=A[['pressure']], PF=A[['pressureFlag']])
+#  D <- data.frame(T=A[["temperature"]], TF=A[["temperatureFlag"]],
+#                  S=A[["salinity"]], SF=A[["salinityFlag"]],
+#                  P=A[["pressure"]], PF=A[["pressureFlag"]])
 
 ## ---- message=FALSE, error=FALSE, warning=FALSE, eval=FALSE-------------------
 #  clean <- applyQC(argos)
@@ -43,16 +43,16 @@ if (packageVersion("oce") > "1.2.0") {
     library(argoFloats)
     raw <- readProfiles(system.file("extdata", "SD5903586_001.nc", package="argoFloats"))
     adjusted <- useAdjusted(raw)
-    rawOxygen <- unlist(raw[['oxygen']])
-    rawPressure <- unlist(raw[['pressure']])
-    adjustedOxygen <- unlist(adjusted[['oxygen']])
-    adjustedPressure <- unlist(adjusted[['pressure']])
+    rawOxygen <- unlist(raw[["oxygen"]])
+    rawPressure <- unlist(raw[["pressure"]])
+    adjustedOxygen <- unlist(adjusted[["oxygen"]])
+    adjustedPressure <- unlist(adjusted[["pressure"]])
     xlim <- range(c(rawOxygen, adjustedOxygen), na.rm=TRUE)
     plot(rawOxygen, rawPressure,
          pch=1, col=1,
          xlim=xlim, ylim=rev(range(rawPressure, na.rm=TRUE)),
          xlab=expression("Raw Oxygen ["*mu*mol/kg*"]"),
-         ylab='Pressure (dbar)')
+         ylab="Pressure (dbar)")
     points(adjustedOxygen, adjustedPressure,
            pch=3, col=2)
     legend("bottomright", pch=c(1,3), col=c(1,2), c("Raw", "Adjusted"))
@@ -82,16 +82,16 @@ if (packageVersion("oce") > "1.2.0") {
 
 ## ----eval=FALSE---------------------------------------------------------------
 #  library(argoFloats)
-#  data('indexSynthetic')
-#  index1 <- subset(indexSynthetic, ID='4900845')
+#  data("indexSynthetic")
+#  index1 <- subset(indexSynthetic, ID="4900845")
 #  profiles <- getProfiles(index1)
 #  argos <- readProfiles(profiles)
-#  plot(argos, which='QC', parameter='temperature')
+#  plot(argos, which="QC", parameter="temperature")
 
 ## ----eval=FALSE---------------------------------------------------------------
 #  library(argoFloats)
-#  data('indexSynthetic')
-#  index1 <- subset(indexSynthetic, ID='4900845')
+#  data("indexSynthetic")
+#  index1 <- subset(indexSynthetic, ID="4900845")
 #  profiles <- getProfiles(index1)
 #  argos <- readProfiles(profiles)
 #  a1 <- argos[[1]]
@@ -101,7 +101,7 @@ if (packageVersion("oce") > "1.2.0") {
 #  library(argoFloats)
 #  # Contrast TS diagrams for raw and flag-handled data
 #  data(indexSynthetic)
-#  index1 <- subset(indexSynthetic, ID='4900845')
+#  index1 <- subset(indexSynthetic, ID="4900845")
 #  argos <- readProfiles(getProfiles(index1))
 #  clean <- applyQC(argos)
 #  par(mfrow=c(1, 2))
@@ -109,22 +109,22 @@ if (packageVersion("oce") > "1.2.0") {
 #  plot(clean, which="TS")
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  summary(unlist(argos[['temperature']]))
-#  summary(unlist(clean[['temperature']]))
-#  summary(unlist(argos[['salinity']]))
-#  summary(unlist(clean[['salinity']]))
+#  summary(unlist(argos[["temperature"]]))
+#  summary(unlist(clean[["temperature"]]))
+#  summary(unlist(argos[["salinity"]]))
+#  summary(unlist(clean[["salinity"]]))
 
 ## ----eval=FALSE---------------------------------------------------------------
 #  library(argoFloats)
 #  raw <- readProfiles(system.file("extdata", "SD5903586_001.nc", package="argoFloats"))
 #  adjusted <- useAdjusted(raw)
-#  rawC <- unlist(raw[['chlorophyllA']])
-#  rawPressure <- unlist(raw[['pressure']])
-#  adjustedC <- unlist(adjusted[['chlorophyllA']])
-#  adjustedPressure <- unlist(adjusted[['pressure']])
-#  plot(rawC, rawPressure, ylim=rev(range(rawPressure, na.rm=TRUE)), pch=16, col='blue', xlab=expression("Raw Chlorophyll A (mg/m3)"), ylab='Pressure (dbar)')
-#  points(adjustedC, adjustedPressure, ylim=rev(range(unlist(adjusted[['pressure']]), na.rm=TRUE)), pch=16, col='red')
-#  legend("bottomright", col=c('blue','red'), c("Raw", "Adjusted"), pch=c(16, 16))
+#  rawC <- unlist(raw[["chlorophyllA"]])
+#  rawPressure <- unlist(raw[["pressure"]])
+#  adjustedC <- unlist(adjusted[["chlorophyllA"]])
+#  adjustedPressure <- unlist(adjusted[["pressure"]])
+#  plot(rawC, rawPressure, ylim=rev(range(rawPressure, na.rm=TRUE)), pch=16, col="blue", xlab=expression("Raw Chlorophyll A (mg/m3)"), ylab="Pressure (dbar)")
+#  points(adjustedC, adjustedPressure, ylim=rev(range(unlist(adjusted[["pressure"]]), na.rm=TRUE)), pch=16, col="red")
+#  legend("bottomright", col=c("blue","red"), c("Raw", "Adjusted"), pch=c(16, 16))
 
 ## ----eval=FALSE---------------------------------------------------------------
 #  plot(rawC, adjustedC,
