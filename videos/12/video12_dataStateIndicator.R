078269fd8c5a1607f4b4b5f46abdf3df498ef67d
Jaimie Harbin <jm810136@dal.ca>
2020-11-12 11:04:55 -0400

    tweaks to video code

--- videos/12/video12_dataStateIndicator.R
+++ videos/12/video12_dataStateIndicator.R
@@ -7,8 +7,8 @@ library(argoFloats)
 index1 <- subset(getIndex(), circle=list(latitude=12.25, longitude=64.33, radius=40))
 argos <- readProfiles(getProfiles(index1))
 table(unlist(argos[["dataStateIndicator"]]))
-indicator <- c("3C", "3B", "2C+", "2C","2B+", "2B", "1A", "0A")
 argos2 <- subset(argos, dataStateIndicator="2C")
+argos2[['dataStateIndicator']]
 
 # Dan
 
