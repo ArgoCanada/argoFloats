e5ad3d278c7745c1d3f696d7b8e62d17112a7a78
dankelley <kelley.dan@gmail.com>
2020-11-13 11:33:13 -0400

    dsi

--- videos/12/video12_dataStateIndicator.R
+++ videos/12/video12_dataStateIndicator.R
@@ -1,8 +1,3 @@
-## Note current code doesn't work. Need to fix subsetting by dataStateIndicator
-## and handle more than one column in summary plot
-
-# Jaimie 
-
 library(argoFloats)
 ## Arabian Sea
 index1 <- subset(getIndex(), circle=list(latitude=12.25, longitude=64.33, radius=40))
@@ -12,11 +7,3 @@ table(unlist(argos1[["dataStateIndicator"]]))
 ## Get and test subset
 argos2 <- subset(argos1, dataStateIndicator="2C")
 argos2[["dataStateIndicator"]]
-
-# Dan
-
-par(mar=c(2,2,1,1))
-plot(argos, which="map", bathymetry=FALSE)
-lon2 <- unlist(argos2[["longitude"]])
-lat2 <- unlist(argos2[["latitude"]])
-points(lon2, lat2, col=4, cex=2)
