e13436b0126868dad533ea4c091f41882c22ad2c
Jaimie Harbin <jm810136@dal.ca>
2020-11-16 08:35:48 -0400

    sweeden talk

--- sandbox/jlh/32_sweeden/01_sweeden.R
+++ sandbox/jlh/32_sweeden/01_sweeden.R
@@ -1,9 +1,12 @@
-if (!interactive()) pdf("01_sweeden.pdf", width=7, height=3.5, pointsize=11)
 library(argoFloats)
 if (!exists("ai"))
        ai <- getIndex()
-index1 <- subset(ai, circle=list(longitude=-60.85,latitude=46.24, radius=200))
-index2 <- subset(ai, circle=list(latitude=59.33, longitude=18.06, radius=100))
+index1 <- subset(ai, circle=list(longitude=-60.85,latitude=46.24, radius=200)) # Canada
+index2 <- subset(ai, circle=list(latitude=59.33, longitude=18.06, radius=100)) # Sweeden
 merge <- merge(index1,index2)
 plot(merge)
-if (!interactive()) dev.off()
+argos1 <- readProfiles(getProfiles(index1))
+argos2 <- readProfiles(getProfiles(index2))
+par(mfrow=c(1,2))
+plot(argos1, which="profile", profileControl=list(parameter="temperature"),type="p", sub="Nova Scotia, Canada")
+plot(argos2, which="profile", profileControl=list(parameter="temperature"), type="p", sub="Stockholm, Sweeden")
