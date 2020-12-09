59b426520901cb850c798993893da4e6fc8c368e
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2020-12-09 14:27:47 -0400

    add `argoDefaultServer()`

--- R/defaults.R
+++ R/defaults.R
@@ -1,21 +1,33 @@
 ## vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
 
-#' Get the default Argo cache directory
+#' Get Default Values
 #'
 #' By default, files downloaded from the Argo mirror are placed in
 #' `~/data/argo`. You can configure where these files are stored by setting
 #' `options(argoFloat.destdir="path/to/argo")`. To persist this value
-#' between sessions you can place this value in your `.Rprofile`.
+#' between sessions you can place this value in your `.Rprofile`. Similarly,
+#' the default value for `server` in [getIndex()] is `"ifremer-https"`, which
+#' can be configured by setting `options(argoFloat.server = "https://...")`.
 #'
 #' @return A character value containing a path to the default Argo data directory.
 #' @export
 #'
 #' @examples
 #' argoDefaultDestdir()
+#' argoDefaultServer()
+#' argoDefaultIndexAge()
+#' argoDefaultProfileAge()
 #'
 argoDefaultDestdir <- function()
 {
     argoOptionValue("argoFloats.destdir", "R_ARGOFLOATS_DESTDIR", "~/data/argo")
+}
+
+#' @rdname argoDefaultDestdir
+#' @export
+argoDefaultServer <- function()
+{
+    argoOptionValue("argoFloats.server", "R_ARGOFLOATS_SERVER", "ifremer-https")
 }
 
 #' @rdname argoDefaultDestdir
