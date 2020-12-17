3afaaf12ee09eecf4b05800dd08c7049cb88401e
dankelley <kelley.dan@gmail.com>
2020-12-17 12:01:05 -0400

    getIndex() download speeded up by 5% (saving ~4s)
    I instrumented the code, and got as follows.
    
    $ Rscript -e "library(argoFloats);i<-getIndex(age=0)"
    
    Downloading 'https://data-argo.ifremer.fr/ar_index_global_prof.txt.gz'
       user  system elapsed
     22.793   0.505  23.437
    
    $ Rscript -e "library(argoFloats);i<-getIndex(age=0)"
    
    Downloading 'https://data-argo.ifremer.fr/ar_index_global_prof.txt.gz'
       user  system elapsed
     18.559   0.340  18.917

--- R/get.R
+++ R/get.R
@@ -332,14 +332,18 @@ getIndex <- function(filename="core",
     header <- first[hash]
     lastHash <- tail(hash, 1)
     names <- strsplit(first[1 + lastHash], ",")[[1]]
-   #if (grepl("merge", filename)) {
-   #     names <- c("file", "date", "latitude", "longitude", "ocean",
-   #                "profiler_type", "institution", "parameters",
-   #                "param_data_mode", "date_update")
-   #     argoFloatsDebug(debug, "Skipping (flawed) header in the merged file.\n", sep="")
-   # }
+    ##if (grepl("merge", filename)) {
+    ##     names <- c("file", "date", "latitude", "longitude", "ocean",
+    ##                "profiler_type", "institution", "parameters",
+    ##                "param_data_mode", "date_update")
+    ##     argoFloatsDebug(debug, "Skipping (flawed) header in the merged file.\n", sep="")
+    ## }
     argoFloatsDebug(debug, "Reading index file contents (can be slow).\n", sep="")
-    index <- read.csv(destfileTemp, skip=2 + lastHash, col.names=names, stringsAsFactors=FALSE)
+    ##    print(system.time(
+    index <- read.csv(destfileTemp, skip=2 + lastHash, col.names=names, stringsAsFactors=FALSE, colClasses="character")
+    ##))
+    index$latitude <- as.numeric(index$latitude)
+    index$longitude <- as.numeric(index$longitude)
     argoFloatsDebug(debug, "Setting out-of-range longitude and latitude to NA.\n")
     if ("latitude" %in% names(index)) {
         index$latitude[abs(index$latitude) > 90] <- NA
