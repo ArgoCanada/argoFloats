8b781986db69edb494d2ec1a8cbcacf0c75bc68a
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-01-15 09:19:18 -0400

    vectorize downloadWithRetries

--- R/download.R
+++ R/download.R
@@ -22,7 +22,6 @@
 #' or `NA`, if there was a problem with the download.
 #'
 ## @importFrom curl curl_download
-#' @importFrom utils unzip
 #'
 #' @export
 #'
@@ -30,45 +29,27 @@
 downloadWithRetries <- function(url, destdir=argoDefaultDestdir(), destfile=NULL, quiet=FALSE,
                                 age=argoDefaultProfileAge(), retries=3, debug=0)
 {
-    if (!requireNamespace("curl", quietly=TRUE))
-        stop("must install.packages(\"curl\") for downloadWithRetries() to work")
-    if (missing(url))
-        stop("must specify url")
-    if (length(destdir) > 1)
-        stop("destdir must be of length 1")
     retries <- max(1, as.integer(retries))
-    argoFloatsDebug(debug, "downloadWithRetries(\n",
-                    style="bold", sep="", unindent=1)
-    argoFloatsDebug(debug, "    url='", paste(url, collapse="', '"), "',\n",
-                    style="bold", sep="", unindent=1)
-    argoFloatsDebug(debug, "    destdir='", destdir, "',\n",
-                    style="bold", sep="", unindent=1)
-    argoFloatsDebug(debug, "    destfile='", paste(destfile, collapse="', '"), "',\n",
-                    style="bold", sep="", unindent=1)
-    argoFloatsDebug(debug, "    quiet=", quiet, ", retries=", retries, ") {\n",
-                    style="bold", sep="", unindent=1)
-    n <- length(url)
-    if (length(destfile) != n)
-        stop("length(url)=", n, " must equal length(destfile)=", length(destfile))
-    for (i in 1:n) {
-        destination <- paste0(destdir, "/", destfile[i])
+    if (length(destfile) != length(url))
+        stop("length(url)=", length(destfile), " must equal length(destfile)=", length(destfile))
+    
+    if (length(url) == 0)
+        return(character(0))
+    
+    destination <- character(length(destfile))
+    for (i in seq_along(url)) {
+        destination[i] <- paste0(destdir, "/", destfile[i])
         if (file.exists(destination)) {
             destinationAge <- (as.integer(Sys.time()) - as.integer(file.info(destination)$mtime)) / 86400 # in days
-            argoFloatsDebug(debug, "destinationAge=", destinationAge, "\n", sep="")
         }
-        if (file.exists(destination) && destinationAge < age) {
-            argoFloatsDebug(debug, "Skipping \"", destination, "\" because it already exists and its age, ", destinationAge, "d, is under specified age=", age, "d\n", sep="")
-        } else {
+        if (!file.exists(destination) || (destinationAge < age)) {
             success <- FALSE
             for (trial in seq_len(1 + retries)) {
                 if (!quiet) message(sprintf("Downloading '%s'", url))
                 t <- try(curl::curl_download(url=url, destfile=destination), silent=TRUE)
                 if (inherits(t, "try-error") && any(grepl("application callback", t))) {
                     stop(t)
-                } else if (inherits(t, "try-error")) {
-                    argoFloatsDebug(debug, "failed download from \"", url, "\" ", if (trial < (1+retries)) "(will try again)\n" else "(final attempt)\n", sep="")
                 } else {
-                    argoFloatsDebug(debug, "successful download from \"", url, "\"\n", sep="")
                     success <- TRUE
                     break
                 }
@@ -78,17 +59,8 @@ downloadWithRetries <- function(url, destdir=argoDefaultDestdir(), destfile=NULL
                     message("failed download '", url, "'\n  after ", retries, " attempts.\n  Try running getIndex(age=0) to refresh the index, in case a file name changed.")
                 return(NA)
             }
-            if (1 == length(grep(".zip$", destfile[i]))) {
-                destinationClean <- gsub(".zip$", "", destination[i])
-                unzip(destination[i], exdir=destinationClean)
-                destination[i] <- destinationClean
-                argoFloatsDebug(debug, "  Downloaded and unzipped into '", destination[i], "'\n", sep="")
-            } else {
-                argoFloatsDebug(debug, "  Downloaded file stored as '", destination[i], "'\n", sep="")
-            }
         }
     }
-    argoFloatsDebug(debug, "} # downloadWithRetries()\n", style="bold", unindent=1)
+    
     destination
 }
-
