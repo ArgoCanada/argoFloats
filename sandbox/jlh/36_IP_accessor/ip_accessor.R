2771a8aaa33bc11ec79c728ce7682486077011b1
Jaimie Harbin <jm810136@dal.ca>
2021-01-18 08:40:30 -0400

    jlh sandbox

--- sandbox/jlh/36_IP_accessor/ip_accessor.R
+++ sandbox/jlh/36_IP_accessor/ip_accessor.R
@@ -2,9 +2,10 @@ library(argoFloats)
 data(index)
 index1 <- subset(index, 1:10)
 argos <- readProfiles(getProfiles(index1))
-for (i in seq_along(argos)) {
-history <- unlist(argos[["historyAction"]][[i]])
-keep <- grepl("IP", history)
-print(keep)
+for (i in seq_along(argos[["argos"]])) {
+    historyList <- argos[["historyAction"]][[i]][1,]
 }
-x@data$index <- x@data$index[keep, ]
+historyList <- lapply(argos[["historyAction"]], function(h) argos[["historyAction"]][[i]][1,])
+keeph <- grepl("IP", historyList)
+index1@data$index <- index1@data$index[keeph, ]
+
