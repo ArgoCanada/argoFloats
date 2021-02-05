a4acad1432a07a7e608a0d925bb0d37d6db16a44
Jaimie Harbin <jm810136@dal.ca>
2021-02-05 12:34:27 -0400

    Adding widgets to qcApp

---
+++ R/qcApp.R
@@ -0,0 +1,118 @@
+library(shiny)
+
+uiQCApp <- fluidPage(
+
+                shiny::tabsetPanel(type="tab",
+                                   shiny::tabPanel("Main", value=1),
+                                   shiny::tabPanel("Single Float", value=2),
+                                   id="tabselected"),
+                shiny::fluidRow(
+                                shiny::uiOutput(outputId="UIwidget")),
+
+                shiny::fluidRow(
+                                shiny::uiOutput(outputId="UIfocus")),
+
+                shiny::fluidRow(shiny::uiOutput(outputId="UIsingleQC")),
+                shiny::fluidRow(shiny::uiOutput(outputId="UIplot"))
+
+)
+
+QCAppserver <- shinyServer(function(input,output){
+
+
+                          output$UIfocus <- shiny::renderUI({
+                              if (input$tabselected %in% c(2)) {
+                                  shiny::fluidRow(
+                                                  shiny::column(2, shiny::textInput("ID", "Float ID", value="", width="11em")),
+                                                  shiny::column(2, shiny::textInput("cycle", "Cycle", value="", width="11em")))
+                              }
+                          })
+
+                          output$UIwidget <- shiny::renderUI({
+                              if (input$tabselected %in% c(1, 2)) {
+                                  shiny::fluidRow(shiny::column(2,shiny::span(shiny::HTML(paste("<b style=\"color:black; margin-left:1em;\">  ","qcApp 0.1","</b>")))),
+                                                  shiny::column(2, shiny::actionButton("help", "Help")),
+                                                  shiny::column(2, shiny::actionButton("code", "Code")))}})
+
+                          output$UIsingleQC <- shiny::renderUI({
+                              if (input$tabselected %in% c(2)) {
+                                  shiny::checkboxGroupInput("qc",
+                                                            label="Single float QC",
+                                                            choiceNames=list(shiny::tags$span("applyQC", style="color:black;"),
+                                                                             shiny::tags$span("showQCTests", style="color:black;"),
+                                                                             shiny::tags$span("Diagnostics", style="color:black;")),
+                                                            choiceValues=list("applyQC","showQCTests", "diagnostics"),
+                                                            inline=TRUE)
+                              }
+                          })
+
+                          output$UIplot <- shiny::renderUI({
+                              ## NOTE: The type and colorBy are the same as oceanglider
+                              if (input$tabselected %in% c(1)) {
+
+                                  shiny::fluidRow(
+                                                  shiny::column(3, shiny::selectInput("type", "Plot Type",choices=c("TS <T>"="TS",
+                          "C(t)"="conductivity time-series",
+                          "p(t) <P>"="pressure time-series",
+                          "S(t) <S>"="salinity time-series",
+                          "spiciness(t)"="spiciness time-series",
+                          "T(t)"="temperature time-series",
+                          "tSincePowerOn(t)"="tSincePowerOn time-series",
+                          "C(p)"="conductivity profile",
+                          "density(p)"="density profile",
+                          "S(p)"="salinity profile",
+                          "spiciness(p)"="spiciness profile",
+                          "T(p)"="temperature profile",
+                          "hist(C)"="conductivity histogram",
+                          "hist(p)"="pressure histogram",
+                          "hist(S)"="salinity histogram",
+                          "hist(T)"="temperature histogram"),
+                selected="pressure time-series")),
+
+                                  shiny::column(3, shiny::selectInput(inputId="colorBy",
+                label="Color by",
+                choices=c("distance"="distance",
+                          "latitude"="latitude",
+                          "longitude"="longitude",
+                          "p"="pressure",
+                          "T"="temperature",
+                          "S"="salinity",
+                          "density"="sigma0",
+                          "N2"="N2",
+                          "instability"="instability",
+                          "oxygen",
+                          "navState"="navState",
+                          "tSincePowerOn"="tSincePowerOn",
+                          "spiciness"="spiciness0",
+                          "(none)"="(none)"),
+                selected="(none)")))}
+
+                          })
+
+})
+
+#' Interactive app for analying Argo QC
+#'
+#' The GUI permits user to analyze QC of Argo data. As of 2020-02-05, this was still in early stages of development.
+#'
+#' @param debug integer value that controls how much information `mapApp()` prints
+#' to the console as it works.  The default value of 0 leads to a fairly limited
+#' amount of printing, while higher values lead to more information. This information
+#' can be helpful in diagnosing problems or bottlenecks.
+#'
+#' @examples
+#' if (interactive()) {
+#'     library(argoFloats)
+#'     qcApp()
+#' }
+#'
+#' @author Jaimie Harbin
+#' @importFrom shiny shinyApp shinyOptions
+#' @export
+qcApp <- function(debug=0)
+{
+    debug <- as.integer(max(0, min(debug, 3))) # put in range from 0 to 3
+    if (!requireNamespace("shiny", quietly=TRUE))
+        stop("must install.packages(\"shiny\") for this to work")
+    print(shiny::shinyApp(ui=uiQCApp, server=QCAppserver))
+}
