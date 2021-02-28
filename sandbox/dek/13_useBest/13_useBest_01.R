ae6a945216784a0da08254f3ac9757e00d6f8669
dankelley <kelley.dan@gmail.com>
2021-02-28 13:17:07 -0400

    more weird datasets

--- sandbox/dek/13_useBest/13_useBest_01.R
+++ sandbox/dek/13_useBest/13_useBest_01.R
@@ -1,88 +1,100 @@
+# NOTE 1: As 13_useBest_02.Rmd shows, some files have matrix (multi-column)
+# data, with *different* DATA_MODE values for each.  How should we handle that,
+# in deciding whether to choose raw or adjusted data?
+
 library(argoFloats)
 set.seed(408)
 if (!exists("a")) {
     i <- getIndex(age=10) # no need to get latest and greatest
-    n <- 5 # 200
+    n <- 5#200
     s <- subset(i, sample(seq_len(i[["length"]]), n))
     a <- readProfiles(getProfiles(s))
 }
 
 debug <- 1
 
-useBest <- function(a)
+useBestSingle <- function(a, debug=0)
 {
-    if (!inherits(a, "argoFloats"))
-        stop("'a' must be an object created with argoFloats::readProfiles()")
+    rval <- a
+    fn <- a[["filename"]]
+    typeFromFilename <- switch(substring(gsub(".*/","",fn),1,1), "A"="adjusted", "D"="delayed", "R"="realtime")
+    cat(sprintf("%s (filename suggests %s data)\n", fn, typeFromFilename))
+    varNames <- names(a[["data"]])
+    adjustedNames <- grepl("Adjusted$", varNames)
+    varNamesRaw <- varNames[!adjustedNames]
+    varNamesAdjusted <- varNames[adjustedNames]
+    if (debug > 1) {
+        cat("      varNames: \"", paste(varNames, collapse="\" \""), "\"\n", sep="")
+        cat("      varNamesRaw: \"", paste(varNamesRaw, collapse="\" \""), "\"\n", sep="")
+        cat("      varNamesAdjusted: \"", paste(varNamesAdjusted, collapse="\" \""), "\"\n", sep="")
+    }
+    if ("dataMode" %in% names(a@metadata)) { # core
+        dm <- a@metadata$dataMode[1]
+        cat("      non-BGC dataset since dataMode exists (it is ", a@metadata$dataMode, ")\n", sep="")
+        if (dm == "D") { # SEE NOTE 1 above
+            # FIXME: see note at "A" below.
+            # FIXME: copy flags also.
+            for (name in varNamesRaw) {
+                adjustedName <- paste0(name, "Adjusted")
+                if (adjustedName %in% varNamesAdjusted) {
+                    nok <- sum(is.finite(a@data[[adjustedName]]))
+                    rval@data[[name]] <- a@data[[adjustedName]]
+                    cat("      ", adjustedName, " -> ", name, " (", nok, " finite data)\n", sep="")
+                }
+            }
+        } else if (dm == "R") {
+            weird <- FALSE
+            nok <- 0
+            for (name in varNamesRaw) {
+                adjustedName <- paste0(name, "Adjusted")
+                if (adjustedName %in% varNamesAdjusted) {
+                    nok <- sum(is.finite(a@data[[adjustedName]]))
+                    rval@data[[name]] <- a@data[[adjustedName]]
+                    cat("      ", adjustedName, " -> ", name, " (", nok, " finite data)\n", sep="")
+                    weird <- TRUE
+                }
+            }
+            if (weird) {
+                if (nok) {
+                    cat("      ERROR: 'realtime' file contains non-NA 'adjusted' data.\n")
+                } else {
+                    cat("      WARNING: 'realtime' file contains 'adjusted' data, which seems wrong, but at least they all are NA.\n")
+                }
+            }
+        } else if (dm == "A") {
+            # FIXME: if this will be identical to the "D" case, we'll copy the code there.
+            # FIXME: copy flags also.
+            weird <- TRUE
+            for (name in varNamesRaw) {
+                adjustedName <- paste0(name, "Adjusted")
+                if (adjustedName %in% varNamesAdjusted) {
+                    nok <- sum(is.finite(a@data[[adjustedName]]))
+                    rval@data[[name]] <- a@data[[adjustedName]]
+                    cat("      ", adjustedName, " -> ", name, " (", nok, " finite data)\n", sep="")
+                    weird <- FALSE
+                }
+            }
+            if (weird)
+                cat("    ERROR: 'adjusted' file lacks 'adjusted' data.\n")
+        } else {
+            stop("dataMode must be \"A\", \"R\", or \"D\", not \"", dm, "\"")
+        }
+    } else if ("parameterDataMode" %in% names(a@metadata)) { # BGC
+        dm <- a@metadata$dataMode
+        cat("   BGC dataset (contains \"parameterDataMode\"=\"", paste(dm, collapse="\" \""), "\")\n", sep="")
+    }
+    rval
+}
+
+
+useBest <- function(a, debug=0)
+{
+    if (!inherits(a, "argoFloats")) # FIXME: accept an oce::argo object or list of them
+        stop("'a' must be a list of oce:argo objects, or an object created with argoFloats::readProfiles()")
     rval <- a
     for (i in seq_along(a[["argos"]])) {
-        ai <- a[[i]]
-        fn <- ai[["filename"]]
-        typeFromFilename <- switch(substring(gsub(".*/","",fn),1,1), "A"="adjusted", "D"="delayed", "R"="realtime")
-        message(sprintf("\n%2d. %s (filename suggests %s data)", i, fn, typeFromFilename))
-        varNames <- names(ai[["data"]])
-        adjustedNames <- grepl("Adjusted$", varNames)
-        varNamesRaw <- varNames[!adjustedNames]
-        varNamesAdjusted <- varNames[adjustedNames]
-        if (debug > 1) {
-            message("      varNames: \"", paste(varNames, collapse="\" \""), "\"")
-            message("      varNamesRaw: \"", paste(varNamesRaw, collapse="\" \""), "\"")
-            message("      varNamesAdjusted: \"", paste(varNamesAdjusted, collapse="\" \""), "\"")
-        }
-        if ("dataMode" %in% names(ai@metadata)) { # core
-            dm <- ai@metadata$dataMode[1]
-            message("    core dataset with dataMode=\"", dm, "\"")
-            if (dm == "D") {
-                # FIXME: see note at "A" below.
-                # FIXME: copy flags also.
-                for (name in varNamesRaw) {
-                    adjustedName <- paste0(name, "Adjusted")
-                    if (adjustedName %in% varNamesAdjusted) {
-                        nok <- sum(is.finite(ai@data[[adjustedName]]))
-                        ai@data[[name]] <- ai@data[[adjustedName]]
-                        message("      ", adjustedName, " -> ", name, " (", nok, " finite data)")
-                    }
-                }
-            } else if (dm == "R") {
-                weird <- FALSE
-                nok <- 0
-                for (name in varNamesRaw) {
-                    adjustedName <- paste0(name, "Adjusted")
-                    if (adjustedName %in% varNamesAdjusted) {
-                        nok <- sum(is.finite(ai@data[[adjustedName]]))
-                        ai@data[[name]] <- ai@data[[adjustedName]]
-                        message("      ", adjustedName, " -> ", name, " (", nok, " finite data)")
-                        weird <- TRUE
-                    }
-                }
-                if (weird) {
-                    if (nok) {
-                        message("    ERROR: 'realtime' file contains non-NA 'adjusted' data.")
-                    } else {
-                        message("    WARNING: 'realtime' file contains 'adjusted' data, which seems wrong, but at least they all are NA.")
-                    }
-                }
-            } else if (dm == "A") {
-                # FIXME: if this will be identical to the "D" case, we'll copy the code there.
-                # FIXME: copy flags also.
-                weird <- TRUE
-                for (name in varNamesRaw) {
-                    adjustedName <- paste0(name, "Adjusted")
-                    if (adjustedName %in% varNamesAdjusted) {
-                        nok <- sum(is.finite(ai@data[[adjustedName]]))
-                        ai@data[[name]] <- ai@data[[adjustedName]]
-                        message("      ", adjustedName, " -> ", name, " (", nok, " finite data)")
-                        weird <- FALSE
-                    }
-                }
-                if (weird)
-                    message("    ERROR: 'adjusted' file lacks 'adjusted' data.")
-            } else {
-                stop("dataMode must be \"A\", \"R\", or \"D\", not \"", dm, "\"")
-            }
-        } else if ("parameterDataMode" %in% names(ai@metadata)) { # BGC
-            dm <- ai@metadata$dataMode
-            message("   BGC dataset (contains \"parameterDataMode\"=\"", paste(dm, collapse="\" \""), "\")")
-        }
+        cat(sprintf("\n%2d. ", i))
+        rval@data$argos[[i]] <- useBestSingle(a[[i]], debug=debug)
     }
     rval
 }
--- sandbox/dek/13_useBest/README.md
+++ sandbox/dek/13_useBest/README.md
@@ -1,1 +1,25 @@
 See https://github.com/ArgoCanada/argoFloats/issues/408
+
+# Issues.
+
+## multiple DATA_MODES
+
+As shown below, some (possibly many) files have data **columns** that are of
+unequal modes.  Should useBest() work differently on each column?
+
+```
+ 3. /Users/kelley/Dropbox/data/argo/D6902829_047.nc (filename suggests delayed data)
+      non-BGC dataset since dataMode exists (it is DRRRRRR)
+      pressureAdjusted -> pressure (976 finite data)
+      temperatureAdjusted -> temperature (976 finite data)
+      salinityAdjusted -> salinity (976 finite data)
+```
+```
+6. /Users/kelley/Dropbox/data/argo/D5904013_153.nc (filename suggests delayed data)
+      non-BGC dataset since dataMode exists (it is DAD)
+      pressureAdjusted -> pressure (1293 finite data)
+      temperatureAdjusted -> temperature (1293 finite data)
+      conductivityAdjusted -> conductivity (958 finite data)
+      salinityAdjusted -> salinity (1039 finite data)
+```
+
