cfd661ce1c89618055c2531a32d597cf6e8d8d37
dankelley <kelley.dan@gmail.com>
2021-03-02 14:25:41 -0400

    rough work for new useAdjusted

--- sandbox/dek/13_useBest/13_useBest_03.R
+++ sandbox/dek/13_useBest/13_useBest_03.R
@@ -1,8 +1,115 @@
-# NOTE 1: As 13_useBest_02.Rmd shows, some files have matrix (multi-column)
-# data, with *different* DATA_MODE values for each.  How should we handle that,
-# in deciding whether to choose raw or adjusted data?
+rm(list=ls()) # FIXME: helps during development
 
 library(argoFloats)
+
+useAdjustedSingleNEW <- function(argo, fallback="NA", debug=0)
+{
+    if (!(inherits(argo, "oce") && inherits(argo, "argo")))
+        stop("First argument must be an oce::argo object")
+    res <- argo
+    fn <- argo[["filename"]]
+    typeFromFilename <- switch(substring(gsub(".*/","",fn),1,1), "A"="adjusted", "D"="delayed", "R"="realtime")
+    cat(sprintf("%s (%s?)", gsub(".*/", "", fn), typeFromFilename))
+    if (debug > 0 && "dataMode" %in% names(argo@metadata))
+        cat(" DATA_MODE=", paste(argo@metadata$dataMode, collapse=" "), "\n", sep="")
+    varNames <- names(argo[["data"]])
+    adjustedNames <- grepl("Adjusted$", varNames)
+    varNamesRaw <- varNames[!adjustedNames]
+    varNamesAdjusted <- varNames[adjustedNames]
+
+    flagNames <- names(argo@metadata$flags)
+    adjustedFlagNames <- grepl("Adjusted$", flagNames)
+    flagNamesRaw <- flagNames[!adjustedFlagNames]
+    flagNamesAdjusted <- flagNames[adjustedFlagNames]
+    if (debug > 1) {
+        cat("        varNames:          ", paste(varNames, collapse=" "), "\n", sep="")
+        cat("        varNamesRaw:       ", paste(varNamesRaw, collapse=" "), "\n", sep="")
+        cat("        varNamesAdjusted:  ", paste(varNamesAdjusted, collapse=" "), "\n", sep="")
+        cat("        flagNames:         ", paste(flagNames, collapse=" "), "\n", sep="")
+        cat("        flagNamesRaw:      ", paste(flagNamesRaw, collapse=" "), "\n", sep="")
+        cat("        flagNamesAdjusted: ", paste(flagNamesAdjusted, collapse=" "), "\n", sep="")
+    }
+    if ("dataMode" %in% names(argo@metadata)) { # core
+        dm <- argo@metadata$dataMode[1]
+        cat("      non-BGC dataset since dataMode exists\n", sep="")
+        for (name in varNamesRaw) {
+            adjustedName <- paste0(name, "Adjusted")
+            if (adjustedName %in% varNamesAdjusted) {
+                nok <- sum(is.finite(argo@data[[adjustedName]]))
+                res@data[[name]] <- argo@data[[adjustedName]]
+                if (debug > 0)
+                    cat("      data: ", adjustedName, " -> ", name, " (", nok, " finite data)\n", sep="")
+            }
+        }
+        for (name in flagNamesRaw) {
+            adjustedName <- paste0(name, "Adjusted")
+            if (adjustedName %in% flagNamesAdjusted) {
+                res@metadata$flags[[name]] <- argo@metadata[[adjustedName]]
+                if (debug > 0)
+                    cat("      flag: ", adjustedName, " -> ", name, "\n", sep="")
+            }
+        }
+    } else if ("parameterDataMode" %in% names(argo@metadata)) { # BGC
+        dm <- argo@metadata$dataMode
+        if (debug > 0)
+            cat("   BGC dataset (contains \"parameterDataMode\"=\"", paste(dm, collapse="\" \""), "\")\n", sep="")
+    }
+    res@processingLog <- oce::processingLogAppend(res@processingLog,
+                                                  paste0("useAdjustedSingle(argos, fallback=\"", fallback, "\", debug=", debug, ")\n"))
+    res
+}
+
+#' Switch [[ Focus to Adjusted data
+#'
+#' FIXME: intro para.
+#'
+#' There are two cases.  *Case 1*: If `fallback` is `"NA"`
+#' (the default), then the adjusted values are returned, even if they
+#' are all `NA`. This is a cautious approach that should yield only
+#' data of high quality.  *Case 2*: if `fallback` is `"raw"`, then the data in
+#' the adjusted columns are examined one by one. If the values in a given
+#' column are all `NA`, then the parameter's raw values are returned.
+#' However, if any of the values in a given adjusted column are non-`NA`,
+#' then that whole adjusted column is returned, ignoring the raw values.
+#' Thus, the `"raw"` method provides a rough estimate even for dubious
+#' profiles.
+#'
+#' @param argo an [`argoFloats-class`] object, as read by [readProfiles()].
+#'
+#' This function returns a version of its first argument for which the enclosed
+#' [oce::argo-class] objects have been modified in a way that makes
+#' future uses of \code{\link{[[,argoFloats-method}}
+#' focus (entirely or preferentially) on the *adjusted* data,
+#' not the original data.
+#'
+#' @param argo an [`argoFloats-class`] object, as read by [readProfiles()].
+#'
+#' @param fallback a character value indicating what to do if all the adjusted
+#' values for a particular parameter-profile pair are `NA`.
+#'
+#' @author Dan Kelley
+#'
+#' @export
+useAdjustedNEW <- function(argos, fallback="NA", debug=0)
+{
+    if (!inherits(argos, "argoFloats"))
+        stop("'argos' must be an argoFloats object")
+    if ("argos" != argos@metadata$type)
+        stop("'argos' must be an argoFloats object created with argoFloats::readProfiles()")
+    if (!fallback %in% c("NA", "raw"))
+        stop("fallback value \"", fallback, "\" is not understood.  It must be \"NA\" or \"raw\"")
+    res <- argos
+    argoList <- argos[["argos"]]
+    for (i in seq_along(argoList)) {
+        if (debug > 0)
+            cat(sprintf("\n%2d. ", i))
+        res@data$argos[[i]] <- useAdjustedSingleNEW(argoList[[i]], fallback=fallback, debug=debug)
+    }
+    DAN<<-match.call()
+    res@processingLog <- oce::processingLogAppend(res@processingLog, paste(deparse(match.call()), sep="", collapse=""))
+    res
+}
+
 set.seed(408)
 if (!exists("a")) {
     i <- getIndex(age=10) # no need to get latest and greatest
@@ -11,101 +118,5 @@ if (!exists("a")) {
     a <- readProfiles(getProfiles(s))
 }
 
-debug <- 1
-
-useBestSingle <- function(a, debug=0)
-{
-    if (!(inherits(a, "oce") && inherits(a, "argo")))
-        stop("First argument must be an oce::argo object")
-    res <- a
-    fn <- a[["filename"]]
-    typeFromFilename <- switch(substring(gsub(".*/","",fn),1,1), "A"="adjusted", "D"="delayed", "R"="realtime")
-    cat(sprintf("%s (%s?)", gsub(".*/", "", fn), typeFromFilename))
-    if ("dataMode" %in% names(a@metadata))
-        cat(" DATA_MODE=", paste(a@metadata$dataMode, collapse=" "), "\n", sep="")
-    varNames <- names(a[["data"]])
-    adjustedNames <- grepl("Adjusted$", varNames)
-    varNamesRaw <- varNames[!adjustedNames]
-    varNamesAdjusted <- varNames[adjustedNames]
-    if (debug > 1) {
-        cat("      varNames: \"", paste(varNames, collapse="\" \""), "\"\n", sep="")
-        cat("      varNamesRaw: \"", paste(varNamesRaw, collapse="\" \""), "\"\n", sep="")
-        cat("      varNamesAdjusted: \"", paste(varNamesAdjusted, collapse="\" \""), "\"\n", sep="")
-    }
-    if ("dataMode" %in% names(a@metadata)) { # core
-        dm <- a@metadata$dataMode[1]
-        cat("      non-BGC dataset since dataMode exists\n", sep="")
-        if (dm == "D") { # SEE NOTE 1 above
-            # FIXME: see note at "A" below.
-            # FIXME: copy flags also.
-            for (name in varNamesRaw) {
-                adjustedName <- paste0(name, "Adjusted")
-                if (adjustedName %in% varNamesAdjusted) {
-                    nok <- sum(is.finite(a@data[[adjustedName]]))
-                    res@data[[name]] <- a@data[[adjustedName]]
-                    cat("      ", adjustedName, " -> ", name, " (", nok, " finite data)\n", sep="")
-                }
-            }
-        } else if (dm == "R") {
-            weird <- FALSE
-            nok <- 0
-            for (name in varNamesRaw) {
-                adjustedName <- paste0(name, "Adjusted")
-                if (adjustedName %in% varNamesAdjusted) {
-                    nok <- sum(is.finite(a@data[[adjustedName]]))
-                    res@data[[name]] <- a@data[[adjustedName]]
-                    cat("      ", adjustedName, " -> ", name, " (", nok, " finite data)\n", sep="")
-                    weird <- TRUE
-                }
-            }
-            if (weird) {
-                if (nok) {
-                    cat("      ERROR: 'realtime' file contains non-NA 'adjusted' data.\n")
-                } else {
-                    cat("      WARNING: 'realtime' file contains 'adjusted' data, which seems wrong, but at least they all are NA.\n")
-                }
-            }
-        } else if (dm == "A") {
-            # FIXME: if this will be identical to the "D" case, we'll copy the code there.
-            # FIXME: copy flags also.
-            weird <- TRUE
-            for (name in varNamesRaw) {
-                adjustedName <- paste0(name, "Adjusted")
-                if (adjustedName %in% varNamesAdjusted) {
-                    nok <- sum(is.finite(a@data[[adjustedName]]))
-                    res@data[[name]] <- a@data[[adjustedName]]
-                    cat("      ", adjustedName, " -> ", name, " (", nok, " finite data)\n", sep="")
-                    weird <- FALSE
-                }
-            }
-            if (weird)
-                cat("    ERROR: 'adjusted' file lacks 'adjusted' data.\n")
-        } else {
-            stop("dataMode must be \"A\", \"R\", or \"D\", not \"", dm, "\"")
-        }
-    } else if ("parameterDataMode" %in% names(a@metadata)) { # BGC
-        dm <- a@metadata$dataMode
-        cat("   BGC dataset (contains \"parameterDataMode\"=\"", paste(dm, collapse="\" \""), "\")\n", sep="")
-    }
-    res@processingLog <- oce::processingLogAppend(res@processingLog, paste(deparse(match.call()), sep="", collapse=""))
-    res
-}
-
-
-useBest <- function(a, debug=0)
-{
-    if (!inherits(a, "argoFloats"))
-        stop("'a' must be an argoFloats object")
-    if ("argos" != a@metadata$type)
-        stop("'a' must be an argoFloats object created with argoFloats::readProfiles()")
-    res <- a
-    for (i in seq_along(a[["argos"]])) {
-        cat(sprintf("\n%2d. ", i))
-        res@data$argos[[i]] <- useBestSingle(a[[i]], debug=debug)
-    }
-    res@processingLog <- oce::processingLogAppend(res@processingLog, paste(deparse(match.call()), sep="", collapse=""))
-    res
-}
-
-b <- useBest(a)
+b <- useAdjustedNEW(a, fallback="NA", debug=1)
 
