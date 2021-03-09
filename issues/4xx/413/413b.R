f546787ce992589fcb4e9a8aa36dddb20b1e566e
dankelley <kelley.dan@gmail.com>
2021-03-09 17:56:18 -0400

    enlarge test

--- issues/4xx/413/413b.R
+++ issues/4xx/413/413b.R
@@ -2,7 +2,7 @@ library(argoFloats)
 set.seed(413)
 i <- getIndex()
 len <- i[["length"]]
-ntrials <- 2 # 200
+ntrials <- 200
 s <- subset(i, sample(seq_len(len), ntrials))
 raw <- readProfiles(getProfiles(s))
 adj <- useAdjusted(raw, fallback=TRUE, debug=2)
