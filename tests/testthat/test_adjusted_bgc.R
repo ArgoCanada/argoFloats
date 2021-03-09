dc820b7d18c16c669e807ec3c9bb5ec689e2d3d1
dankelley <kelley.dan@gmail.com>
2021-03-09 10:35:11 -0400

    test useAdjusted() with bgc (single-profile case)

--- tests/testthat/test_adjusted_bgc.R
+++ tests/testthat/test_adjusted_bgc.R
@@ -4,11 +4,11 @@ context("useAdjusted() on BGC (synthetic) data")
 
 test_that("useAdjusted() on BGC (synthetic) delayed-mode data", {
           a <- readProfiles(system.file("extdata", "SD5903586_001.nc", package="argoFloats"), quiet=TRUE)
-          aNA <- useAdjusted(a)
-})
-
-
-test_that("useAdjusted() on BGC (synthetic) delayed-mode data", {
-          a <- readProfiles(system.file("extdata", "SR2902204_131.nc", package="argoFloats"), quiet=TRUE)
-          aNA <- useAdjusted(a)
+          a2 <- useAdjusted(a)
+          expect_equal(-6.69775332282, mean(a[[1]][["oxygen"]] - a2[[1]][["oxygen"]],na.rm=TRUE))
+          # Create fake data, in which oxygen mode is "R", so we will fall back to raw
+          aFake <- a
+          aFake@data$argos[[1]]@metadata$parameterDataMode <- "DDDRARR" # Original is "DDDDARR"
+          a3 <- useAdjusted(aFake, fallback=TRUE)
+          expect_equal(a3[[1]][["oxygen"]], a[[1]][["oxygen"]])
 })
