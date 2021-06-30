2d9d585eea5570e2f798bc776cc971a6804b8077
Jaimie Harbin <jm810136@dal.ca>
2021-06-30 08:27:19 -0300

    remove hover message from settings tab

--- R/mapApp.R
+++ R/mapApp.R
@@ -267,7 +267,7 @@ serverMapApp <- function(input, output, session)
             return(lastHoverMessage)
         x <- input$hover$x
         y <- input$hover$y
-        if (is.null(x))
+        if (is.null(x) && input$tabselected == 1)
             return("Hover mouse in plot to see locations; click-slide to select regions.")
         lonstring <- ifelse(x < 0, sprintf("%.2fW", abs(x)), sprintf("%.2fE", x))
         latstring <- ifelse(y < 0, sprintf("%.2fS", abs(y)), sprintf("%.2fN", y))
