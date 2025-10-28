381a2b913c85c7e12a88976a14f68b207976ba38
dankelley <kelley.dan@gmail.com>
2025-10-28 11:40:53 -0300

    Skip downloadWithRetries() tests (re. issue #605)

--- tests/testthat/test_download.R
+++ tests/testthat/test_download.R
@@ -1,6 +1,8 @@
 ## vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
 data("index")
 
+test_downloadWithRetries <- FALSE # see discussion on issue #605
+
 context("bgc download")
 
 test_that("getIndex()", {
@@ -32,113 +34,102 @@ test_that("readProfiles()", {
     argos <- expect_silent(readProfiles(filename))
 })
 
-test_that("downloadWithRetries() can download files", {
-    skip_if_offline()
-    destDir <- tempfile()
-    dir.create(destDir)
-    urls <- rep("https://httpbin.org/get", 3)
-    destFile <- c("file1", "file2", "file3")
-    expect_identical(
-        downloadWithRetries(urls, destDir, destFile, quiet = TRUE),
-        file.path(destDir, destFile)
-    )
-    expect_true(all(file.exists(file.path(destDir, destFile))))
-    unlink(destDir, recursive = TRUE)
-})
-
-test_that("downloadWithRetries() can download zero files", {
-    expect_identical(
-        downloadWithRetries(character(), tempfile(), character()),
-        character()
-    )
-})
-
-test_that("downloadWithRetries() skips downloads based on age", {
-    destDir <- tempfile()
-    dir.create(destDir)
-    destFile <- file.path(destDir, "empty_file_that_exists.txt")
-    expect_true(file.create(destFile))
-    bogusUrlThatWillFail <- "this is not a url"
-
-    expect_identical(
-        downloadWithRetries(bogusUrlThatWillFail, destDir, basename(destFile),
-            age = Inf
-        ),
-        destFile
-    )
-
-    unlink(destDir, recursive = TRUE)
-})
-
-test_that("downloadWithRetries() sets destination to NA on failure", {
-    bogusUrlThatWillFail <- "this is not a url"
-
-    expect_message(
+if (test_downloadWithRetries) {
+    test_that("downloadWithRetries() can download files", {
+        skip_if_offline()
+        destDir <- tempfile()
+        dir.create(destDir)
+        urls <- rep("https://httpbin.org/get", 3)
+        destFile <- c("file1", "file2", "file3")
         expect_identical(
-            downloadWithRetries(bogusUrlThatWillFail, tempfile(), "dest", quiet = FALSE),
-            NA_character_
-        ),
-        "failed download"
-    )
-})
+            downloadWithRetries(urls, destDir, destFile, quiet = TRUE),
+            file.path(destDir, destFile)
+        )
+        expect_true(all(file.exists(file.path(destDir, destFile))))
+        unlink(destDir, recursive = TRUE)
+    })
 
-
-test_that("downloadWithRetries() can download files with async=TRUE", {
-    skip_if_offline()
-    destDir <- tempfile()
-    dir.create(destDir)
-    urls <- rep("https://httpbin.org/get", 3)
-    destFile <- c("file1", "file2", "file3")
-    expect_identical(
-        downloadWithRetries(urls, destDir, destFile,
-            quiet = TRUE, async = TRUE
-        ),
-        file.path(destDir, destFile)
-    )
-    expect_true(all(file.exists(file.path(destDir, destFile))))
-    unlink(destDir, recursive = TRUE)
-})
-
-test_that("downloadWithRetries() skips downloads based on age with async=TRUE", {
-    destDir <- tempfile()
-    dir.create(destDir)
-    destFile <- file.path(destDir, "empty_file_that_exists.txt")
-    expect_true(file.create(destFile))
-    bogusUrlThatWillFail <- "this is not a url"
-
-    expect_identical(
-        downloadWithRetries(bogusUrlThatWillFail, destDir, basename(destFile),
-            age = Inf, async = TRUE
-        ),
-        destFile
-    )
-
-    unlink(destDir, recursive = TRUE)
-})
-
-test_that("downloadWithRetries() sets destination to NA on failure with async=TRUE", {
-    bogusUrlThatWillFail <- "this is not a url"
-
-    expect_message(
+    test_that("downloadWithRetries() can download zero files", {
         expect_identical(
-            downloadWithRetries(bogusUrlThatWillFail, tempfile(), "dest",
-                quiet = FALSE, async = TRUE
+            downloadWithRetries(character(), tempfile(), character()),
+            character()
+        )
+    })
+
+    test_that("downloadWithRetries() skips downloads based on age", {
+        destDir <- tempfile()
+        dir.create(destDir)
+        destFile <- file.path(destDir, "empty_file_that_exists.txt")
+        expect_true(file.create(destFile))
+        bogusUrlThatWillFail <- "this is not a url"
+
+        expect_identical(
+            downloadWithRetries(bogusUrlThatWillFail, destDir, basename(destFile),
+                age = Inf
             ),
-            NA_character_
-        ),
-        "failed download"
-    )
-})
+            destFile
+        )
+
+        unlink(destDir, recursive = TRUE)
+    })
+
+    test_that("downloadWithRetries() sets destination to NA on failure", {
+        bogusUrlThatWillFail <- "this is not a url"
+
+        expect_message(
+            expect_identical(
+                downloadWithRetries(bogusUrlThatWillFail, tempfile(), "dest", quiet = FALSE),
+                NA_character_
+            ),
+            "failed download"
+        )
+    })
 
 
-## > ## Comment this out because servers change, so we cannot predict success or failure
-## > test_that("getProfile() with single out-of-date URL",
-## >           {
-## >               skip_if_not(hasArgoTestCache())
-## >               data(index)
-## >               s <- expect_message(subset(index, 778), "Kept 1 cycles")
-## >               p <- expect_output(getProfiles(s, skip=FALSE), "|===")
-## >               p <- expect_output(getProfiles(s, skip=TRUE), "|===")
-## >               p <- expect_output(getProfiles(s),"|===") # default is skip=TRUE
-## >           }
-## > )
+    test_that("downloadWithRetries() can download files with async=TRUE", {
+        skip_if_offline()
+        destDir <- tempfile()
+        dir.create(destDir)
+        urls <- rep("https://httpbin.org/get", 3)
+        destFile <- c("file1", "file2", "file3")
+        expect_identical(
+            downloadWithRetries(urls, destDir, destFile,
+                quiet = TRUE, async = TRUE
+            ),
+            file.path(destDir, destFile)
+        )
+        expect_true(all(file.exists(file.path(destDir, destFile))))
+        unlink(destDir, recursive = TRUE)
+    })
+
+    test_that("downloadWithRetries() skips downloads based on age with async=TRUE", {
+        destDir <- tempfile()
+        dir.create(destDir)
+        destFile <- file.path(destDir, "empty_file_that_exists.txt")
+        expect_true(file.create(destFile))
+        bogusUrlThatWillFail <- "this is not a url"
+
+        expect_identical(
+            downloadWithRetries(bogusUrlThatWillFail, destDir, basename(destFile),
+                age = Inf, async = TRUE
+            ),
+            destFile
+        )
+
+        unlink(destDir, recursive = TRUE)
+    })
+
+    test_that("downloadWithRetries() sets destination to NA on failure with async=TRUE", {
+        bogusUrlThatWillFail <- "this is not a url"
+
+        expect_message(
+            expect_identical(
+                downloadWithRetries(bogusUrlThatWillFail, tempfile(), "dest",
+                    quiet = FALSE, async = TRUE
+                ),
+                NA_character_
+            ),
+            "failed download"
+        )
+    })
+}
